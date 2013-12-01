25c3351fa0c8d4b5ca8299dae7c811d4c04c97d6
Svalorzen <svalorzen@gmail.com>
2013-12-01 01:58:40 +0100

    Updated wiki

--- search/all_72.js
+++ search/all_72.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['rand_5f',['rand_',['../classAIToolbox_1_1MDP_1_1DynaQ.html#a7e1492cc81936574d9dc6ac40691b7cf',1,'AIToolbox::MDP::DynaQ::rand_()'],['../classAIToolbox_1_1MDP_1_1Model.html#ae3c8e086d8cfac9600203ff37afa8b82',1,'AIToolbox::MDP::Model::rand_()'],['../classAIToolbox_1_1PolicyInterface.html#a1e54b57eb7b9c0ddb054baab0b8b9383',1,'AIToolbox::PolicyInterface::rand_()']]],
   ['randomdistribution_5f',['randomDistribution_',['../classAIToolbox_1_1EpsilonPolicy.html#a25501a697a3998748a6a0d9ce26e585f',1,'AIToolbox::EpsilonPolicy']]],
+  ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
   ['record',['record',['../classAIToolbox_1_1Experience.html#afd25442deea3212b36327caf5999b9a6',1,'AIToolbox::Experience']]],
   ['reset',['reset',['../classAIToolbox_1_1Experience.html#a02709808ccac7c11c2ae2df535553dc4',1,'AIToolbox::Experience']]],
   ['rewards_5f',['rewards_',['../classAIToolbox_1_1MDP_1_1Model.html#a129a2e19fe2a76461603e6b9001a1ece',1,'AIToolbox::MDP::Model']]],
--- search/files_72.js
+++ search/files_72.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
   ['rlmodel_2ehpp',['RLModel.hpp',['../RLModel_8hpp.html',1,'']]]
 ];
