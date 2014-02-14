06f8249bfd1c365dc42406739cc497676a0f00ce
Svalorzen <svalorzen@gmail.com>
2014-02-14 02:52:05 +0100

    Documentation for Seeder

--- include/AIToolbox/Impl/Seeder.hpp
+++ include/AIToolbox/Impl/Seeder.hpp
@@ -5,6 +5,13 @@
 
 namespace AIToolbox {
     namespace Impl {
+        /**
+         * @brief This class is an internal class used to seed all random engines in the library.
+         *
+         * To avoid seeding all generators with a single seed equal to the current time, only
+         * this class is setup with the time seed, while all others are seeded with numbers
+         * generated from this class to obtain maximum randomness.
+         */
         class Seeder {
             public:
                 static unsigned getSeed();
