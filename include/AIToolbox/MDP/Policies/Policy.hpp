ba78ed34d0cd23eb4005187b619be5844265ccef
Svalorzen <svalorzen@gmail.com>
2014-05-02 00:36:07 +0200

    Moved MDP Policy into MDP namespace

--- include/AIToolbox/MDP/Policies/Policy.hpp
+++ include/AIToolbox/MDP/Policies/Policy.hpp
@@ -1,5 +1,5 @@
-#ifndef AI_TOOLBOX_POLICY_HEADER_FILE
-#define AI_TOOLBOX_POLICY_HEADER_FILE
+#ifndef AI_TOOLBOX_MDP_POLICY_HEADER_FILE
+#define AI_TOOLBOX_MDP_POLICY_HEADER_FILE
 
 #include <vector>
 #include <tuple>
@@ -9,153 +9,154 @@
 #include <AIToolbox/PolicyInterface.hpp>
 
 namespace AIToolbox {
-    /**
-     * @brief This class represents a full policy.
-     *
-     * Building this object is expensive, so it should be done
-     * mostly when it is known that the final solution won't
-     * change again. Otherwise you may want to build a wrapper
-     * around some data to extract the policy dynamically.
-     */
-    class Policy : public PolicyInterface<size_t> {
-        public:
-            using PolicyTable = Table2D;
+    namespace MDP {
+        /**
+         * @brief This class represents a full policy.
+         *
+         * Building this object is expensive, so it should be done
+         * mostly when it is known that the final solution won't
+         * change again. Otherwise you may want to build a wrapper
+         * around some data to extract the policy dynamically.
+         */
+        class Policy : public PolicyInterface<size_t> {
+            public:
+                using PolicyTable = Table2D;
 
-            /**
-             * @brief Basic constrctor.
-             *
-             * This constructor initializes the internal policy table so that
-             * each action in each state has the same probability of being
-             * chosen (random policy). This class guarantees that at any point
-             * the internal policy is a true probability distribution, i.e.
-             * for each state the sum of the probabilities of chosing an action
-             * sums up to 1.
-             *
-             * @param s The number of states of the world.
-             * @param a The number of actions available to the agent.
-             */
-            Policy(size_t s, size_t a);
+                /**
+                 * @brief Basic constrctor.
+                 *
+                 * This constructor initializes the internal policy table so that
+                 * each action in each state has the same probability of being
+                 * chosen (random policy). This class guarantees that at any point
+                 * the internal policy is a true probability distribution, i.e.
+                 * for each state the sum of the probabilities of chosing an action
+                 * sums up to 1.
+                 *
+                 * @param s The number of states of the world.
+                 * @param a The number of actions available to the agent.
+                 */
+                Policy(size_t s, size_t a);
 
-            Policy(const PolicyInterface & p);
+                Policy(const PolicyInterface & p);
 
-            /**
-             * @brief This function chooses a random action for state s, following the policy distribution.
-             *
-             * @param s The sampled state of the policy.
-             *
-             * @return The chosen action.
-             */
-            virtual size_t sampleAction(const size_t & s) const override;
+                /**
+                 * @brief This function chooses a random action for state s, following the policy distribution.
+                 *
+                 * @param s The sampled state of the policy.
+                 *
+                 * @return The chosen action.
+                 */
+                virtual size_t sampleAction(const size_t & s) const override;
 
-            /**
-             * @brief This function returns the probability of taking the specified action in the specified state.
-             *
-             * @param s The selected state.
-             * @param a The selected action.
-             *
-             * @return The probability of taking the selected action in the specified state.
-             */
-            virtual double getActionProbability(const size_t & s, size_t a) const override;
+                /**
+                 * @brief This function returns the probability of taking the specified action in the specified state.
+                 *
+                 * @param s The selected state.
+                 * @param a The selected action.
+                 *
+                 * @return The probability of taking the selected action in the specified state.
+                 */
+                virtual double getActionProbability(const size_t & s, size_t a) const override;
 
-            /**
-             * @brief This function sets the policy for a particular state.
-             *
-             * This function copies correctly sized container into the policy,
-             * normalizing it so that it sums to 1. If the size of the container
-             * is incorrect, an std::invalid_argument is thrown.
-             *
-             * The container needs to support size() and begin/end iterators.
-             * The elements of the container must be convertible to double, and the
-             * results of the convertions MUST BE positive.
-             *
-             * @tparam T The type of the input container.
-             * @param s The state where the policy is being set.
-             * @param container The input container.
-             */
-            template <typename T>
-            void setStatePolicy(size_t s, const T & container);
+                /**
+                 * @brief This function sets the policy for a particular state.
+                 *
+                 * This function copies correctly sized container into the policy,
+                 * normalizing it so that it sums to 1. If the size of the container
+                 * is incorrect, an std::invalid_argument is thrown.
+                 *
+                 * The container needs to support size() and begin/end iterators.
+                 * The elements of the container must be convertible to double, and the
+                 * results of the convertions MUST BE positive.
+                 *
+                 * @tparam T The type of the input container.
+                 * @param s The state where the policy is being set.
+                 * @param container The input container.
+                 */
+                template <typename T>
+                void setStatePolicy(size_t s, const T & container);
 
-            /**
-             * @brief This function returns a copy of a particular slice of the policy.
-             *
-             * @param s The requested state.
-             *
-             * @return The probabilities of choosing each action in state s.
-             */
-            std::vector<double> getStatePolicy(size_t s) const;
+                /**
+                 * @brief This function returns a copy of a particular slice of the policy.
+                 *
+                 * @param s The requested state.
+                 *
+                 * @return The probabilities of choosing each action in state s.
+                 */
+                std::vector<double> getStatePolicy(size_t s) const;
 
-            /**
-             * @brief This function sets the policy for a particular state.
-             *
-             * This function represents an easier way to set a probability
-             * of 1 to execute a given action in a particular state, and
-             * setting all other probabilities to 0.
-             *
-             * @param s The state where the policy is being set.
-             * @param a The action that must be chosen in state s.
-             */
-            void setStatePolicy(size_t s, size_t a);
+                /**
+                 * @brief This function sets the policy for a particular state.
+                 *
+                 * This function represents an easier way to set a probability
+                 * of 1 to execute a given action in a particular state, and
+                 * setting all other probabilities to 0.
+                 *
+                 * @param s The state where the policy is being set.
+                 * @param a The action that must be chosen in state s.
+                 */
+                void setStatePolicy(size_t s, size_t a);
 
-            /**
-             * @brief This function enables inspection of the internal policy.
-             *
-             * @return A constant reference to the internal policy.
-             */
-            const PolicyTable & getPolicy() const;
+                /**
+                 * @brief This function enables inspection of the internal policy.
+                 *
+                 * @return A constant reference to the internal policy.
+                 */
+                const PolicyTable & getPolicy() const;
 
-            /**
-             * @brief Prints the policy to a stream.
-             *
-             * This function differs from operator<<() in that it
-             * avoids printing all probabilities that equal 0. This
-             * results in a much more readable and smaller file.
-             *
-             * The format of the output file is:
-             *
-             * state_number action_number probability
-             *
-             * @param os The stream where the policy is printed.
-             */
-            void prettyPrint(std::ostream & os) const;
-        private:
-            PolicyTable policy_;
+                /**
+                 * @brief Prints the policy to a stream.
+                 *
+                 * This function differs from operator<<() in that it
+                 * avoids printing all probabilities that equal 0. This
+                 * results in a much more readable and smaller file.
+                 *
+                 * The format of the output file is:
+                 *
+                 * state_number action_number probability
+                 *
+                 * @param os The stream where the policy is printed.
+                 */
+                void prettyPrint(std::ostream & os) const;
+            private:
+                PolicyTable policy_;
 
-            friend std::istream& operator>>(std::istream &is, Policy &);
-    };
+                friend std::istream& operator>>(std::istream &is, Policy &);
+        };
 
-    /**
-     * @brief This function reads a policy from a file.
-     *
-     * This function reads files that have been outputted through
-     * operator>>(). If not enough values can be extracted from
-     * the stream, the function stops and the input policy is
-     * not modified. In addition, it checks whether the probability
-     * values are within 0 and 1.
-     *
-     * State and actions are also verified, and this function does
-     * not accept a randomly shuffled policy file. The file must
-     * be sorted by state, and each state must be sorted by action.
-     *
-     * As a layer of additional precaution, the function normalizes
-     * the policy once it has been read, to assure true probability
-     * distribution on the internal policy.
-     *
-     * @param is The stream were the policy is being read from.
-     * @param p The policy that is being assigned.
-     *
-     * @return The input stream.
-     */
-    std::istream& operator>>(std::istream &is, Policy & p);
+        /**
+         * @brief This function reads a policy from a file.
+         *
+         * This function reads files that have been outputted through
+         * operator>>(). If not enough values can be extracted from
+         * the stream, the function stops and the input policy is
+         * not modified. In addition, it checks whether the probability
+         * values are within 0 and 1.
+         *
+         * State and actions are also verified, and this function does
+         * not accept a randomly shuffled policy file. The file must
+         * be sorted by state, and each state must be sorted by action.
+         *
+         * As a layer of additional precaution, the function normalizes
+         * the policy once it has been read, to assure true probability
+         * distribution on the internal policy.
+         *
+         * @param is The stream were the policy is being read from.
+         * @param p The policy that is being assigned.
+         *
+         * @return The input stream.
+         */
+        std::istream& operator>>(std::istream &is, Policy & p);
 
-    template <typename T>
-    void Policy::setStatePolicy(size_t s, const T & container) {
-        if ( container.size() != getA() ) throw std::invalid_argument("Container to copy has the wrong size.");
+        template <typename T>
+        void Policy::setStatePolicy(size_t s, const T & container) {
+            if ( container.size() != getA() ) throw std::invalid_argument("Container to copy has the wrong size.");
 
-        double norm = static_cast<double>(std::accumulate(std::begin(container), std::end(container), 0.0));
-        decltype(policy_)::reference ref = policy_[s]; // This is needed because policy_[s] by itself is a temporary (const), and "saving" it enables the use of transform. Boost magic!
-        std::transform(std::begin(container), std::end(container), std::begin(ref), [norm](decltype(*std::begin(container)) t){ return t/norm; });
+            double norm = static_cast<double>(std::accumulate(std::begin(container), std::end(container), 0.0));
+            decltype(policy_)::reference ref = policy_[s]; // This is needed because policy_[s] by itself is a temporary (const), and "saving" it enables the use of transform. Boost magic!
+            std::transform(std::begin(container), std::end(container), std::begin(ref), [norm](decltype(*std::begin(container)) t){ return t/norm; });
+        }
     }
-
 }
 
 #endif
--- src/MDP/Policies/Policy.cpp
+++ src/MDP/Policies/Policy.cpp
@@ -6,88 +6,90 @@
 #include <AIToolbox/ProbabilityUtils.hpp>
 
 namespace AIToolbox {
-    Policy::Policy(size_t s, size_t a) : PolicyInterface<size_t>(s, a), policy_(boost::extents[S][A])
-    {
-        // Random policy is default
-        std::fill(policy_.data(), policy_.data() + policy_.num_elements(), 1.0/getA());
-    }
-
-    Policy::Policy(const PolicyInterface & p) : PolicyInterface(p.getS(), p.getA()), policy_(boost::extents[S][A])
-    {
-        for ( size_t s = 0; s < S; ++s )
-            for ( size_t a = 0; a < A; ++a )
-                policy_[s][a] = p.getActionProbability(s, a);
-    }
-
-    size_t Policy::sampleAction(const size_t & s) const {
-        return sampleProbability(policy_[s], A, rand_);
-    }
-
-    double Policy::getActionProbability(const size_t & s, size_t a) const {
-        return policy_[s][a];
-    }
-
-    std::vector<double> Policy::getStatePolicy( size_t s ) const {
-        std::vector<double> statePolicy(A);
-
-        std::copy(std::begin(policy_[s]), std::end(policy_[s]), std::begin(statePolicy));
-
-        return statePolicy;
-    }
-
-    void Policy::setStatePolicy(size_t s, size_t a) {
-        for ( size_t ax = 0; ax < A; ++ax )
-            policy_[s][ax] = static_cast<double>( ax == a );
-    }
-
-    const Policy::PolicyTable & Policy::getPolicy() const {
-        return policy_;
-    }
-
-    void Policy::prettyPrint(std::ostream & os) const {
-        for ( size_t s = 0; s < S; ++s ) {
-            for ( size_t a = 0; a < A; ++a ) {
-                if ( policy_[s][a] )
-                    os << s << "\t" << a << "\t" << std::fixed << policy_[s][a] << "\n";
-            }
+    namespace MDP {
+        Policy::Policy(size_t s, size_t a) : PolicyInterface<size_t>(s, a), policy_(boost::extents[S][A])
+        {
+            // Random policy is default
+            std::fill(policy_.data(), policy_.data() + policy_.num_elements(), 1.0/getA());
         }
-    }
 
-    std::istream& operator>>(std::istream &is, Policy &p) {
-        size_t S = p.getS();
-        size_t A = p.getA();
+        Policy::Policy(const PolicyInterface & p) : PolicyInterface(p.getS(), p.getA()), policy_(boost::extents[S][A])
+        {
+            for ( size_t s = 0; s < S; ++s )
+                for ( size_t a = 0; a < A; ++a )
+                    policy_[s][a] = p.getActionProbability(s, a);
+        }
 
-        Policy policy(S, A);
+        size_t Policy::sampleAction(const size_t & s) const {
+            return sampleProbability(policy_[s], A, rand_);
+        }
 
-        size_t scheck, acheck;
+        double Policy::getActionProbability(const size_t & s, size_t a) const {
+            return policy_[s][a];
+        }
 
-        bool fail = false;
-        for ( size_t s = 0; s < S; ++s ) {
-            for ( size_t a = 0; a < A; ++a ) {
-                if ( ! ( is >> scheck >> acheck >> policy.policy_[s][a] ) ) {
-                    std::cerr << "AIToolbox: Could not read policy data.\n"; 
-                    fail = true;
-                }
-                else if ( policy.policy_[s][a] < 0.0 || policy.policy_[s][a] > 1.0 ) {
-                    std::cerr << "AIToolbox: Input policy data contains non-probability values.\n";
-                    fail = true;
-                }
-                else if ( scheck != s || acheck != a ) {
-                    std::cerr << "AIToolbox: Input policy data is not sorted by state and action.\n";
-                    fail = true;
-                }
-                if ( fail ) {
-                    is.setstate(std::ios::failbit);
-                    return is;
+        std::vector<double> Policy::getStatePolicy( size_t s ) const {
+            std::vector<double> statePolicy(A);
+
+            std::copy(std::begin(policy_[s]), std::end(policy_[s]), std::begin(statePolicy));
+
+            return statePolicy;
+        }
+
+        void Policy::setStatePolicy(size_t s, size_t a) {
+            for ( size_t ax = 0; ax < A; ++ax )
+                policy_[s][ax] = static_cast<double>( ax == a );
+        }
+
+        const Policy::PolicyTable & Policy::getPolicy() const {
+            return policy_;
+        }
+
+        void Policy::prettyPrint(std::ostream & os) const {
+            for ( size_t s = 0; s < S; ++s ) {
+                for ( size_t a = 0; a < A; ++a ) {
+                    if ( policy_[s][a] )
+                        os << s << "\t" << a << "\t" << std::fixed << policy_[s][a] << "\n";
                 }
             }
         }
-        // Read succeeded
-        for ( size_t s = 0; s < S; ++s ) {
-            // Sanitization: Assign and normalize everything.
-            p.setStatePolicy(s, policy.policy_[s]);
-        }
 
-        return is;
+        std::istream& operator>>(std::istream &is, Policy &p) {
+            size_t S = p.getS();
+            size_t A = p.getA();
+
+            Policy policy(S, A);
+
+            size_t scheck, acheck;
+
+            bool fail = false;
+            for ( size_t s = 0; s < S; ++s ) {
+                for ( size_t a = 0; a < A; ++a ) {
+                    if ( ! ( is >> scheck >> acheck >> policy.policy_[s][a] ) ) {
+                        std::cerr << "AIToolbox: Could not read policy data.\n"; 
+                        fail = true;
+                    }
+                    else if ( policy.policy_[s][a] < 0.0 || policy.policy_[s][a] > 1.0 ) {
+                        std::cerr << "AIToolbox: Input policy data contains non-probability values.\n";
+                        fail = true;
+                    }
+                    else if ( scheck != s || acheck != a ) {
+                        std::cerr << "AIToolbox: Input policy data is not sorted by state and action.\n";
+                        fail = true;
+                    }
+                    if ( fail ) {
+                        is.setstate(std::ios::failbit);
+                        return is;
+                    }
+                }
+            }
+            // Read succeeded
+            for ( size_t s = 0; s < S; ++s ) {
+                // Sanitization: Assign and normalize everything.
+                p.setStatePolicy(s, policy.policy_[s]);
+            }
+
+            return is;
+        }
     }
 }
