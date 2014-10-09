01b92f1055a75f0eb0e3697dd709b32cda4a5e48
Svalorzen <svalorzen@gmail.com>
2014-10-10 00:57:55 +0200

    Introducing WitnessLP_clp
    This is an alternative to the lp_solve solver. However, at the moment
    this class is not efficient at all, since it does a lot of reallocations
    internally since I have not figured out how to force it to keep memory
    allocated inside. At the same time, it might be nice for people to have
    at least an alternative implementation for the LP.
    
    The CMake build file is already ready to use it, you just need to
    uncomment the new parts that were added with this commit.

---
+++ include/AIToolbox/POMDP/Algorithms/Utils/WitnessLP_clp.hpp
@@ -0,0 +1,98 @@
+#ifndef AI_TOOLBOX_POMDP_WITNESS_LP_CLP_HEADER_FILE
+#define AI_TOOLBOX_POMDP_WITNESS_LP_CLP_HEADER_FILE
+
+#include <cstddef>
+#include <memory>
+#include <vector>
+
+#include <AIToolbox/POMDP/Types.hpp>
+
+#include <coin/ClpSimplex.hpp>
+
+namespace AIToolbox {
+    namespace POMDP {
+        /**
+         * @brief This class implements easy-to-use facilities to do linear programming.
+         *
+         * This particular implementation of the class uses lp_solve to do linear
+         * programming.
+         *
+         * This class is meant to help finding witness points by solving the linear
+         * programming needed. As such, it contains a linear programming problem where
+         * constraints can be set. This class automatically sets the simplex constraint,
+         * where a found belief point needs to sum up to one.
+         *
+         * Optimal constraints can be progressively added as soon as found. When a
+         * new constraint needs to be tested to see if a witness is available, the
+         * findWitness() function can be called.
+         */
+        class WitnessLP_clp {
+            public:
+                /**
+                 * @brief Basic constructor.
+                 *
+                 * This initializes lp_solve structures.
+                 *
+                 * @param S The number of states in the world.
+                 */
+                WitnessLP_clp(size_t S);
+
+                /**
+                 * @brief This function adds a new optimal constraint to the LP, which will not be removed unless the LP is reset.
+                 *
+                 * @param v The optimal constraint to add.
+                 */
+                void addOptimalRow(const std::vector<double> & v);
+
+                /**
+                 * @brief This function solves the currently set LP.
+                 *
+                 * This function tries to solve the underlying LP, and
+                 * returns whether a solution has been found. If it is
+                 * it also returns the witness belief point which satisfies
+                 * the solution.
+                 *
+                 * @return A pair of whether a solution has been found, and an eventual Belief with the solution.
+                 */
+                std::tuple<bool, POMDP::Belief> findWitness(const std::vector<double> & v);
+
+                /**
+                 * @brief This function resets the LP and reserves space for a certain amount of rows to avoid reallocations.
+                 *
+                 * @param rows The max number of constraints for the LP.
+                 */
+                void resetAndAllocate(size_t rows);
+
+            private:
+                /**
+                 * @brief This function adds a constraint row in the LP.
+                 *
+                 * This class works as a stack of constraints: one can
+                 * only add and remove constraints at the bottom of the
+                 * stack. This works out fine for the work this class is
+                 * designed to do, which is finding witness points with
+                 * respect to a set of already optimal constraints which
+                 * will not need to be removed.
+                 *
+                 * @param v The constraint coefficients.
+                 * @param constrType The constraint type.
+                 */
+                void pushRow(const std::vector<double> & v, double min, double max);
+
+                /**
+                 * @brief This function removes a single constraint from the LP in a LIFO fashion.
+                 */
+                void popRow();
+
+                size_t S;
+                int cols_;
+                size_t maxRows_, currRow_;
+                ClpSimplex lp;
+
+                std::unique_ptr<int   []> indeces;
+                std::unique_ptr<double[]> row;
+        };
+    }
+}
+
+#endif
---
+++ src/POMDP/Algorithms/Utils/WitnessLP_clp.cpp
@@ -0,0 +1,134 @@
+#include <AIToolbox/POMDP/Algorithms/Utils/WitnessLP_clp.hpp>
+
+#include <iterator>
+#include <iostream>
+
+namespace AIToolbox {
+    namespace POMDP {
+        WitnessLP_clp::WitnessLP_clp(size_t s) : S(s), cols_(s+2), indeces(new int[cols_]), row(new double[cols_]) {
+            // Verbosity
+            lp.setLogLevel(0);
+            // lp.setPersistenceFlag(1);
+            // Initialize indeces
+            std::iota(indeces.get(), indeces.get() + cols_, 0);
+            /*
+             * Here we setup the part of the lp that never changes (at least with this number of states)
+             * Our constraints are of the form
+             *
+             * b0 >= 0
+             * b1 >= 0
+             * ...
+             * b0 + b1 + ... + bn = 1.0
+             * (v[0] - best[0][0]) * b0 + (v[1] - best[0][1]) * b1 + ... - delta >= 0
+             * (v[0] - best[1][0]) * b0 + (v[1] - best[1][1]) * b1 + ... - delta >= 0
+             * ...
+             *
+             * In particular we know that:
+             *
+             * 1) The simplex constraint is the only constraint that never changes,
+             * so we are going to set it up now.
+             *
+             * 2) The other constraints can be rewritten as follows:
+             *
+             *       v[0] * b0 +       v[1] * b1 + ... - K          = 0
+             * best[0][0] * b0 + best[0][1] * b1 + ... - K - delta <= 0
+             * best[1][0] * b0 + best[1][1] * b1 + ... - K - delta <= 0
+             * ...
+             *
+             * Where basically with the first constraint we are setting K
+             * to the value of v in the final belief, and we are forcing
+             * all other values to be less than that by forcing:
+             *
+             * delta > 0
+             *
+             * The client can then add these and remove them at their discretion.
+             */
+            lp.resize( 0, cols_ );
+
+            // Setup beliefs
+            for ( auto i = 0; i < cols_-2; ++i ) {
+                lp.setObjCoeff(i, 0.0);
+                // Force belief elements between 0 and 1
+                lp.setColumnBounds(i, 0.0, 1.0);
+            }
+
+            // Setup K (unbounded)
+            lp.setObjCoeff(cols_-2, 0.0);
+            lp.setColumnBounds(cols_-2, -COIN_DBL_MAX, +COIN_DBL_MAX);
+
+            // Setup delta
+            lp.setObjCoeff(cols_-1, -1.0); // maximize delta via minimization
+            lp.setColumnBounds(cols_-1, -COIN_DBL_MAX, +COIN_DBL_MAX);
+
+            // CONSTRAINT: This is the simplex constraint (beliefs sum to 1)
+            {
+                // Note: lp_solve reads elements from 1 onwards, so we don't set row[0]
+                for ( int i = 0; i < cols_-2; ++i )
+                    row[i] = 1.0;
+                row[cols_-2] = 0.0; // magic coefficient
+                row[cols_-1] = 0.0; // delta coefficient
+                // The cols_ value doesn't really do anything here, the whole row is read
+                lp.addRow(cols_, indeces.get(), row.get(), 1.0, 1.0);
+            }
+
+            row[cols_-2] = -1.0;
+            row[cols_-1] = +0.0;
+        }
+
+        void WitnessLP_clp::addOptimalRow(const std::vector<double> & v) {
+            // Temporarily set the delta constraint
+            row[cols_-1] = +1.0;
+            pushRow(v, -COIN_DBL_MAX, 0.0); // Less equal than zero
+            row[cols_-1] = 0.0;
+        }
+
+        std::tuple<bool, POMDP::Belief> WitnessLP_clp::findWitness(const std::vector<double> & v) {
+            // Add witness constraint
+            pushRow(v, 0.0, 0.0); // Equal to zero
+
+            lp.dual();
+            lp.primal();
+
+            // TODO: There's a function that gets a pointer to the solution
+            // stored within the LP, maybe use that? (get_ptr_primal_solution)
+            auto results = lp.primalColumnSolution();
+            auto value =   lp.objectiveValue();
+
+            bool isOptimal = lp.isProvenOptimal();
+
+            // Remove test row
+            popRow();
+
+            // We have found a witness point if we have found a belief for which the value
+            // of the supplied ValueFunction is greater than ALL others. Thus we just need
+            // to verify that the variable we have minimized is actually less than 0.
+            if ( ! isOptimal || value >= 0.0 ) {
+                return std::make_pair(false, POMDP::Belief());
+            }
+
+            // For some reason when lp_solve returns the variables it puts them from
+            // 0 to S-1, so here we read accordingly.
+            POMDP::Belief solution(results, results + S);
+            return std::make_pair(true, solution);
+        }
+
+        void WitnessLP_clp::resetAndAllocate(size_t rows) {
+            // Here we simply remove all constraints that are not the simplex
+            // one in order to reset the lp without reallocations.
+            // lp.resize( rows, cols_ );
+            lp.resize( 1, cols_ );
+        }
+
+        void WitnessLP_clp::pushRow(const std::vector<double> & v, double min, double max) {
+            for ( size_t s = 0; s < S; ++s )
+                row[s] = v[s];
+
+            lp.addRow(cols_, indeces.get(), row.get(), min, max);
+        }
+
+        void WitnessLP_clp::popRow() {
+            int numRows = lp.getNumRows() - 1;
+            lp.deleteRows(1, &numRows);
+        }
+    }
+}
