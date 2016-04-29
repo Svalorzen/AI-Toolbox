808215bce8cd626ac759404901b3a6dac95b9cfa
Eugenio Bargiacchi <svalorzen@develer.com>
2016-04-29 22:16:10 +0200

    Fixed bugs in QSoftmaxPolicy

--- src/MDP/Policies/QSoftmaxPolicy.cpp
+++ src/MDP/Policies/QSoftmaxPolicy.cpp
@@ -5,6 +5,8 @@
 #include <AIToolbox/Utils.hpp>
 #include <AIToolbox/ProbabilityUtils.hpp>
 
+#include <iostream>
+
 namespace AIToolbox {
     namespace MDP {
         QSoftmaxPolicy::QSoftmaxPolicy(const QFunction & q, double t) : QPolicyInterface(q), temperature_(t), greedy_(q) {
@@ -12,17 +14,34 @@ namespace AIToolbox {
         }
 
         size_t QSoftmaxPolicy::sampleAction(const size_t & s) const {
-            if ( temperature_ == 0 )
+            if ( temperature_ == 0.0 )
                 return greedy_.sampleAction(s);
 
             Vector actionValues(A);
 
-            for ( size_t a = 0; a < A; ++a )
+            unsigned infinities = 0;
+            for ( size_t a = 0; a < A; ++a ) {
                 actionValues(a) = std::exp(q_(s, a) / temperature_);
+                if ( std::isinf(actionValues(a)) )
+                    infinities++;
+            }
 
-            actionValues.normalize();
+            if (infinities) {
+                auto pickDistribution = std::uniform_int_distribution<unsigned>(0, infinities-1);
+                unsigned selection = pickDistribution(rand_);
 
-            return sampleProbability(A, actionValues, rand_);
+                size_t retval = 0;
+                for ( ; retval < A - 1; ++retval) {
+                    if ( std::isinf(actionValues(retval)) && !selection )
+                        break;
+                    --selection;
+                }
+                return retval;
+            } else {
+                actionValues.normalize();
+
+                return sampleProbability(A, actionValues, rand_);
+            }
         }
 
         double QSoftmaxPolicy::getActionProbability(const size_t & s, size_t a) const {
@@ -31,8 +50,23 @@ namespace AIToolbox {
 
             Vector actionValues(A);
 
-            for ( size_t aa = 0; aa < A; ++aa )
+            unsigned infinities = 0, isAInfinite = 0;
+            for ( size_t aa = 0; aa < A; ++aa ) {
                 actionValues(aa) = std::exp(q_(s, aa) / temperature_);
+                if ( std::isinf(actionValues(aa)) )
+                    infinities++;
+                if ( aa == a && std::isinf(actionValues(aa)) )
+                    isAInfinite = 1;
+            }
+
+            if ( infinities ) {
+                if ( isAInfinite ) return 1.0 / infinities;
+                return 0.0;
+            }
+
+            auto sum = actionValues.sum();
+            if ( checkEqualSmall(sum, 0.0) )
+                return 1.0 / A;
 
             return actionValues(a) / actionValues.sum();
         }
