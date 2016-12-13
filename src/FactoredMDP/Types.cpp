edfd16187cec00215f2afa9e82a4bfa69979a8cc
Svalorzen <svalorzen@gmail.com>
2016-12-13 23:00:21 +0100

    Added docs to type, updated with Factor

--- src/FactoredMDP/Types.cpp
+++
@@ -1,8 +0,0 @@
-#include <AIToolbox/FactoredMDP/Types.hpp>
-
-namespace AIToolbox {
-    namespace FactoredMDP {
-        using FactoredState = std::vector<size_t>;
-        using PartialState = std::vector<std::pair<size_t, size_t>>;
-    }
-}
