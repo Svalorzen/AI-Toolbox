2b692cf1d61720b709232ae0fd24a84dc02ce910
Svalorzen <svalorzen@gmail.com>
2016-12-14 00:22:18 +0100

    Added VariableElimination

---
+++ include/AIToolbox/FactoredMDP/Algorithms/VariableElimination.hpp
@@ -0,0 +1,133 @@
+#ifndef AI_TOOLBOX_VARIABLE_ELIMINATION_HEADER_FILE
+#define AI_TOOLBOX_VARIABLE_ELIMINATION_HEADER_FILE
+
+#include "AIToolbox/ProbabilityUtils.hpp"
+#include "AIToolbox/FactoredMDP/Types.hpp"
+#include "AIToolbox/FactoredMDP/Utils.hpp"
+#include "AIToolbox/FactoredMDP/FactorGraph.hpp"
+
+namespace AIToolbox {
+    namespace FactoredMDP {
+        /**
+         * @brief This class represents the Variable Elimination process.
+         *
+         * This class performs variable elimination on a factor graph. It first
+         * builds the graph starting from a list of QFunctionRules. These rules
+         * are sorted by the agents they affect, and each group is added to a
+         * single factor connected to those agents.
+         *
+         * Each agent is then eliminated from the graph, and all rules
+         * connected to it are processed in order to find out which action the
+         * agent being eliminated should take.
+         *
+         * When all agents have been eliminated, only the optimal rules
+         * containing the best actions remain. The ones that provide the best
+         * reward are joined into a single Action, which is then returned.
+         *
+         * This process is exponential in the maximum number of agents found
+         * attached to the same factor (which could be higher than in the
+         * original graph, as the elimination process can create bigger factors
+         * than the initial ones). However, given that each factor is usually
+         * linked to few agents, and that this process allows avoiding
+         * considering the full factored Action at any one time, it is usually
+         * much faster than a brute-force approach.
+         */
+        class VariableElimination {
+            public:
+                using Rule = std::pair<PartialAction, double>;
+                using Rules = std::vector<Rule>;
+
+                struct Factor{
+                    Rules rules_;
+                };
+
+                using Graph = FactorGraph<Factor>;
+
+                /**
+                 * @brief Basic constructor.
+                 *
+                 * This constructor initializes the internal graph with the
+                 * number of needed agents.
+                 *
+                 * @param a The action space.
+                 */
+                VariableElimination(Action a);
+
+                /**
+                 * @brief This function finds the best Action-value pair for the provided QFunctionRules.
+                 *
+                 * @param rules An iterable object over QFunctionRules.
+                 *
+                 * @return The pair of best Action and its value over the input rules.
+                 */
+                template <typename Iterable>
+                std::pair<Action, double> operator()(const Iterable & rules) {
+                    // Should we reset the graph?
+                    for (const auto & rule : rules) {
+                        auto it = graph_.getFactor(rule.a_.first);
+                        it->f_.rules_.push_back(std::make_pair(rule.a_, rule.value_));
+                    }
+                    return start();
+                }
+
+            private:
+                /**
+                 * @brief This function performs the actual agent elimination process.
+                 *
+                 * For each agent, its adjacent factors, and the agents
+                 * adjacent to those are found. Then all possible action
+                 * combinations between those other agents are tried in order
+                 * to find the best action response be for the agent to be
+                 * eliminated.
+                 *
+                 * All the best responses found are added as Rules to a
+                 * (possibly new) factor adjacent to the adjacent agents.
+                 *
+                 * The process is repeated until all agents are eliminated.
+                 *
+                 * What remains is then joined into a single Action, containing
+                 * the best possible value.
+                 *
+                 * @return The pair for best Action and its value given the internal graph.
+                 */
+                std::pair<Action, double> start();
+
+                /**
+                 * @brief This function performs the elimination of a single agent (and all factors next to it) from the internal graph.
+                 *
+                 * This function adds the resulting best rules which do not
+                 * depend on the eliminated action to the remaining factors.
+                 *
+                 * \sa start()
+                 *
+                 * @param agent The index of the agent to be removed from the graph.
+                 */
+                void removeAgent(size_t agent);
+
+                /**
+                 * @brief This function finds the highest valued rule in the given rules.
+                 *
+                 * @param rules A vector of Rule with at least 1 element.
+                 *
+                 * @return The highest valued rule.
+                 */
+                const Rule & getBestRule(const Rules & rules);
+
+                /**
+                 * @brief This function returns the sum of values of all rules matching the input action.
+                 *
+                 * @param factor The factor to be analyzed.
+                 * @param jointAction The joint action to match each Rule against.
+                 *
+                 * @return The sum of all matching Rules' values.
+                 */
+                double getPayoff(const Factor & factor, const PartialAction & jointAction);
+
+                Graph graph_;
+                Action A;
+                std::vector<Rules> finalFactors_;
+        };
+    }
+}
+
+#endif
---
+++ src/FactoredMDP/Algorithms/VariableElimination.cpp
@@ -0,0 +1,97 @@
+#include <AIToolbox/FactoredMDP/Algorithms/VariableElimination.hpp>
+#include <boost/functional/hash.hpp>
+
+namespace AIToolbox {
+    namespace FactoredMDP {
+        VariableElimination::VariableElimination(Action a) : graph_(a.size()), A(a) {}
+
+        std::pair<Action, double> VariableElimination::start() {
+            // This can possibly be improved with some heuristic ordering
+            while (graph_.agentSize())
+                removeAgent(graph_.agentSize() - 1);
+
+            auto a_v = std::make_pair(Action(A.size()), 0.0);
+            for (const auto & f : finalFactors_) {
+                const auto & pa_v = getBestRule(f);
+
+                a_v.second += pa_v.second;
+                for (size_t i = 0; i < pa_v.first.first.size(); ++i)
+                    a_v.first[pa_v.first.first[i]] = pa_v.first.second[i];
+            }
+
+            return a_v;
+        }
+
+        void VariableElimination::removeAgent(size_t agent) {
+            auto factors = graph_.getNeighbors(agent);
+            auto agents = graph_.getNeighbors(factors);
+
+            Rules newRules;
+            PartialFactorsEnumerator jointActions(A, agents, agent);
+            auto id = jointActions.getFactorToSkipId();
+
+            while (jointActions.isValid()) {
+                auto & jointAction = *jointActions;
+                double bestPayoff = 0.0;
+                size_t bestAction = 0;
+
+                for (size_t agentAction = 0; agentAction < A[agent]; ++agentAction) {
+                    jointAction.second[id] = agentAction;
+
+                    double newPayoff = 0.0;
+                    for (const auto factor : factors)
+                        newPayoff += getPayoff(factor->f_, jointAction); // Crossum x 1
+
+                    // We only select the agent best agent action.
+                    if (newPayoff > bestPayoff) {
+                        bestPayoff = newPayoff;
+                        bestAction = agentAction;
+                    }
+                }
+                if (checkDifferentSmall(bestPayoff, 0.0)) {
+                    jointAction.second[id] = bestAction;
+                    newRules.emplace_back(std::make_pair(jointAction, bestPayoff));
+                }
+                jointActions.advance();
+            }
+
+            for (auto & it : factors)
+                graph_.erase(it);
+            graph_.erase(agent);
+
+            if (newRules.size() == 0) return;
+            if (agents.size() > 1 || agents[0] != agent) {
+                agents.erase(std::remove(std::begin(agents), std::end(agents), agent), std::end(agents));
+
+                auto newFactor = graph_.getFactor(agents);
+                newFactor->f_.rules_.insert(
+                        std::end(newFactor->f_.rules_),
+                        std::make_move_iterator(std::begin(newRules)),
+                        std::make_move_iterator(std::end(newRules))
+                );
+            }
+            else {
+                finalFactors_.push_back(newRules);
+            }
+        }
+
+        const VariableElimination::Rule & VariableElimination::getBestRule(const Rules & rules) {
+            const Rule * bestRule = &rules[0];
+
+            for (const auto & rule : rules)
+                if (rule.second > bestRule->second)
+                    bestRule = &rule;
+
+            return *bestRule;
+        }
+
+        double VariableElimination::getPayoff(const Factor & factor, const PartialAction & jointAction) {
+            // TODO: Put a FactoredContaner in each factor?
+            double result = 0.0;
+            for (const auto & rule : factor.rules_)
+                if (match(jointAction, rule.first))
+                    result += rule.second;
+            return result;
+        }
+    }
+}
