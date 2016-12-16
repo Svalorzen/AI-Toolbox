1a355013a55c8b4dff80062c357d3e7d1015a671
Svalorzen <svalorzen@gmail.com>
2016-12-16 12:11:29 +0100

    Added test for all separate agents in VE

--- test/FactoredMDP/VariableEliminationTests.cpp
+++ test/FactoredMDP/VariableEliminationTests.cpp
@@ -30,3 +30,24 @@ BOOST_AUTO_TEST_CASE( simple_graph ) {
     BOOST_CHECK_EQUAL_COLLECTIONS(std::begin(bestAction_v.first), std::end(bestAction_v.first),
                                   std::begin(solution.first), std::end(solution.first));
 }
+
+BOOST_AUTO_TEST_CASE( all_unconnected_agents ) {
+    std::vector<fm::QFunctionRule> rules {
+        // States, Actions,                     Value
+        {    {},   {{0},    {2}},               4.0},
+        {    {},   {{1},    {0}},               2.0},
+        {    {},   {{2},    {0}},               3.0},
+        {    {},   {{3},    {1}},               7.0},
+    };
+
+    auto solution = std::make_pair(fm::Action{2, 0, 0, 1}, 16.0);
+
+    fm::Action a{3, 2, 3, 4};
+
+    ve v(a);
+    auto bestAction_v = v(rules);
+
+    BOOST_CHECK_EQUAL(bestAction_v.second, solution.second);
+    BOOST_CHECK_EQUAL_COLLECTIONS(std::begin(bestAction_v.first), std::end(bestAction_v.first),
+                                  std::begin(solution.first), std::end(solution.first));
+}
