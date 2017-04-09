4a799d410e5f5993e406c4d5968d5029aec80ce2
Svalorzen <svalorzen@gmail.com>
2017-04-09 02:37:08 +0200

    Removed cruft from QPolicyInterface

--- src/MDP/Policies/QPolicyInterface.cpp
+++ src/MDP/Policies/QPolicyInterface.cpp
@@ -4,5 +4,9 @@ namespace AIToolbox {
     namespace MDP {
         QPolicyInterface::QPolicyInterface(const QFunction & q) :
                 PolicyInterface(q.rows(), q.cols()), q_(q) {}
+
+        const QFunction & QPolicyInterface::getQFunction() const {
+            return q_;
+        }
     }
 }
