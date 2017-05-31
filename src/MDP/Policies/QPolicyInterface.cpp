431b19df9bd8e6f31bd7e42272099490eebaabc7
Svalorzen <svalorzen@gmail.com>
2017-05-31 12:45:30 +0200

    Simplified namespace for mdp qpolicy interface

--- src/MDP/Policies/QPolicyInterface.cpp
+++ src/MDP/Policies/QPolicyInterface.cpp
@@ -1,11 +1,9 @@
 #include <AIToolbox/MDP/Policies/QPolicyInterface.hpp>
 
-namespace AIToolbox {
-    namespace MDP {
-        QPolicyInterface::QPolicyInterface(const QFunction & q) : q_(q) {}
+namespace AIToolbox::MDP {
+    QPolicyInterface::QPolicyInterface(const QFunction & q) : q_(q) {}
 
-        const QFunction & QPolicyInterface::getQFunction() const {
-            return q_;
-        }
+    const QFunction & QPolicyInterface::getQFunction() const {
+        return q_;
     }
 }
