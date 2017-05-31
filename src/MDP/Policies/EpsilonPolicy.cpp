4d7e638f0159600c21b6a89d2a14a9f3625caa54
Svalorzen <svalorzen@gmail.com>
2017-05-31 12:03:49 +0200

    Simplified namespace for mdp epsilon policy

--- src/MDP/Policies/EpsilonPolicy.cpp
+++ src/MDP/Policies/EpsilonPolicy.cpp
@@ -1,27 +1,25 @@
 #include <AIToolbox/MDP/Policies/EpsilonPolicy.hpp>
 
-namespace AIToolbox {
-    namespace MDP {
-        EpsilonPolicy::EpsilonPolicy(const PolicyInterface & p, double epsilon) :
-                PolicyInterface::Base(p.getS(), p.getA()), EpsilonBase(p, epsilon),
-                randomDistribution_(0, this->A-1) {}
+namespace AIToolbox::MDP {
+    EpsilonPolicy::EpsilonPolicy(const PolicyInterface & p, double epsilon) :
+            PolicyInterface::Base(p.getS(), p.getA()), EpsilonBase(p, epsilon),
+            randomDistribution_(0, this->A-1) {}
 
-        size_t EpsilonPolicy::sampleRandomAction() const {
-            return randomDistribution_(rand_);
-        }
+    size_t EpsilonPolicy::sampleRandomAction() const {
+        return randomDistribution_(rand_);
+    }
 
-        double EpsilonPolicy::getRandomActionProbability() const {
-            return 1.0 / A;
-        }
+    double EpsilonPolicy::getRandomActionProbability() const {
+        return 1.0 / A;
+    }
 
-        Matrix2D EpsilonPolicy::getPolicy() const {
-            const auto & wrapped = dynamic_cast<const PolicyInterface &>(policy_);
-            auto p = wrapped.getPolicy();
+    Matrix2D EpsilonPolicy::getPolicy() const {
+        const auto & wrapped = dynamic_cast<const PolicyInterface &>(policy_);
+        auto p = wrapped.getPolicy();
 
-            p *= epsilon_;
-            p.array() += (1.0 - epsilon_) / A;
+        p *= epsilon_;
+        p.array() += (1.0 - epsilon_) / A;
 
-            return p;
-        }
+        return p;
     }
 }
