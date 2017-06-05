7116c06db12cad97495882afba4425a3adcfd06c
Svalorzen <svalorzen@gmail.com>
2017-06-05 13:09:58 +0200

    Simplified namespaces in factored qgreedy policy

--- src/FactoredMDP/Policies/QGreedyPolicy.cpp
+++ src/FactoredMDP/Policies/QGreedyPolicy.cpp
@@ -3,20 +3,18 @@
 #include <AIToolbox/Utils/Core.hpp>
 #include <AIToolbox/FactoredMDP/Algorithms/Utils/VariableElimination.hpp>
 
-namespace AIToolbox {
-    namespace FactoredMDP {
-        QGreedyPolicy::QGreedyPolicy(State s, Action a, const FactoredContainer<QFunctionRule> & q) :
-                Base(std::move(s), std::move(a)), q_(q) {}
+namespace AIToolbox::FactoredMDP {
+    QGreedyPolicy::QGreedyPolicy(State s, Action a, const FactoredContainer<QFunctionRule> & q) :
+            Base(std::move(s), std::move(a)), q_(q) {}
 
-        Action QGreedyPolicy::sampleAction(const State & s) const {
-            VariableElimination ve(A);
-            auto rules = q_.filter(s, 0); // Partial filter
-            return std::get<0>(ve(rules));
-        }
+    Action QGreedyPolicy::sampleAction(const State & s) const {
+        VariableElimination ve(A);
+        const auto rules = q_.filter(s, 0); // Partial filter
+        return std::get<0>(ve(rules));
+    }
 
-        double QGreedyPolicy::getActionProbability(const State & s, const Action & a) const {
-            if (veccmp(a, sampleAction(s)) == 0) return 1.0;
-            return 0.0;
-        }
+    double QGreedyPolicy::getActionProbability(const State & s, const Action & a) const {
+        if (veccmp(a, sampleAction(s)) == 0) return 1.0;
+        return 0.0;
     }
 }
