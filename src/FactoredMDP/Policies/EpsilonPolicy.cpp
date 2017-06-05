7ec6ebdaa737b27a3be465edccf785a7dc0c92ee
Svalorzen <svalorzen@gmail.com>
2017-06-05 13:08:17 +0200

    Simplified namespaces in factored epsilon policy

--- src/FactoredMDP/Policies/EpsilonPolicy.cpp
+++ src/FactoredMDP/Policies/EpsilonPolicy.cpp
@@ -2,28 +2,26 @@
 
 #include <AIToolbox/FactoredMDP/Utils.hpp>
 
-namespace AIToolbox {
-    namespace FactoredMDP {
-        EpsilonPolicy::EpsilonPolicy(const Base::Base & p, double epsilon) :
-                Base::Base(p.getS(), p.getA()), Base(p, epsilon)
-        {
-            randomDistribution_.reserve(getA().size());
-            for (size_t i = 0; i < getA().size(); ++i)
-                randomDistribution_.emplace_back(0, getA()[i] - 1);
-        }
+namespace AIToolbox::FactoredMDP {
+    EpsilonPolicy::EpsilonPolicy(const Base::Base & p, double epsilon) :
+            Base::Base(p.getS(), p.getA()), Base(p, epsilon)
+    {
+        randomDistribution_.reserve(getA().size());
+        for (size_t i = 0; i < getA().size(); ++i)
+            randomDistribution_.emplace_back(0, getA()[i] - 1);
+    }
 
-        Action EpsilonPolicy::sampleRandomAction() const {
-            Action a;
-            a.reserve(getA().size());
+    Action EpsilonPolicy::sampleRandomAction() const {
+        Action a;
+        a.reserve(getA().size());
 
-            for (size_t i = 0; i < getA().size(); ++i)
-                a.push_back(randomDistribution_[i](rand_));
+        for (size_t i = 0; i < getA().size(); ++i)
+            a.push_back(randomDistribution_[i](rand_));
 
-            return a;
-        }
+        return a;
+    }
 
-        double EpsilonPolicy::getRandomActionProbability() const {
-            return 1.0 / factorSpace(A);
-        }
+    double EpsilonPolicy::getRandomActionProbability() const {
+        return 1.0 / factorSpace(A);
     }
 }
