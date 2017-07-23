eedaefce03c4685e37ad6d283da111297fb28912
Svalorzen <svalorzen@gmail.com>
2017-07-24 01:25:01 +0200

    Removed agent mentions in FactorGraph, used more abstract terminology

--- include/AIToolbox/FactoredMDP/FactorGraph.hpp
+++ include/AIToolbox/FactoredMDP/FactorGraph.hpp
@@ -15,12 +15,12 @@ namespace AIToolbox::FactoredMDP {
      *
      * This class allows to store arbitrary data into each factor, and to
      * maintain adjacency lists between the factors and a given number of
-     * agents. The interface is intentionally very simple and tries to do
+     * variables. The interface is intentionally very simple and tries to do
      * very little, in order to allow clients to optimize their use of the
      * graph as much as possible.
      *
      * This class maintains a single factor for any unique combination of
-     * agents. When multiple factors are needed, a single Factor containing
+     * variables. When multiple factors are needed, a single Factor containing
      * a vector of data should suffice.
      *
      * @tparam Factor The Factor class that is stored for each factor.
@@ -28,16 +28,16 @@ namespace AIToolbox::FactoredMDP {
     template <typename Factor>
     class FactorGraph {
         public:
-            struct AgentNode;
+            struct VariableNode;
 
-            using AgentList = std::vector<AgentNode>;
-            using Agents    = std::vector<size_t>;
+            using VariableList = std::vector<VariableNode>;
+            using Variables    = std::vector<size_t>;
 
             class FactorNode {
                 friend class FactorGraph;
 
                 Factor f_;
-                Agents agents_;
+                Variables variables_;
 
                 public:
                     const Factor & getData() const { return f_; }
@@ -53,50 +53,50 @@ namespace AIToolbox::FactoredMDP {
             using iterator = FactorIt;
             using const_iterator = CFactorIt;
 
-            struct AgentNode {
+            struct VariableNode {
                 FactorItList factors_;
             };
 
             /**
              * @brief Basic constructor.
              *
-             * This constructor initializes the agent adjacency list with
-             * the given number of agents. Agents in this class cannot be
+             * This constructor initializes the variable adjacency list with
+             * the given number of variables. Variables in this class cannot be
              * added, only removed.
              *
-             * @param agents The number of agents with which to start the graph.
+             * @param variables The number of variables with which to start the graph.
              */
-            FactorGraph(size_t agents);
+            FactorGraph(size_t variables);
 
             /**
-             * @brief This function returns all factors adjacent to the given agent.
+             * @brief This function returns all factors adjacent to the given variable.
              *
-             * @param agent The agent to look for.
+             * @param variable The variable to look for.
              *
-             * @return A list of iterators pointing at the factors adjacent to the given agent.
+             * @return A list of iterators pointing at the factors adjacent to the given variable.
              */
-            const FactorItList & getNeighbors(size_t agent) const;
+            const FactorItList & getNeighbors(size_t variable) const;
 
             /**
-             * @brief This function returns all agents adjacent to the given factor.
+             * @brief This function returns all variables adjacent to the given factor.
              *
              * @param factor An iterator to the factor to look for.
              *
-             * @return A vector of agents adjacent to the given factor.
+             * @return A vector of variables adjacent to the given factor.
              */
-            const Agents & getNeighbors(FactorIt factor) const;
+            const Variables & getNeighbors(FactorIt factor) const;
 
             /**
-             * @brief This function returns all agents adjacent to the given factor.
+             * @brief This function returns all variables adjacent to the given factor.
              *
              * @param factor A const iterator to the factor to look for.
              *
-             * @return A vector of agents adjacent to the given factor.
+             * @return A vector of variables adjacent to the given factor.
              */
-            const Agents & getNeighbors(CFactorIt factor) const;
+            const Variables & getNeighbors(CFactorIt factor) const;
 
             /**
-             * @brief This function returns all agents adjacent to any of the given factors.
+             * @brief This function returns all variables adjacent to any of the given factors.
              *
              * This function is equivalent to calling the
              * getNeighbors(FactorIt) function multiple times, and merging
@@ -104,12 +104,12 @@ namespace AIToolbox::FactoredMDP {
              *
              * @param factors A list of iterators to the factors to look for.
              *
-             * @return A vector of agents adjacent to any of the given factors.
+             * @return A vector of variables adjacent to any of the given factors.
              */
-            Agents getNeighbors(const FactorItList & factors) const;
+            Variables getNeighbors(const FactorItList & factors) const;
 
             /**
-             * @brief This function returns an iterator to a factor adjacent to the given agents.
+             * @brief This function returns an iterator to a factor adjacent to the given variables.
              *
              * This function may return an iterator to an already existing
              * factor, or if it didn't exist before, to a newly created
@@ -121,11 +121,11 @@ namespace AIToolbox::FactoredMDP {
              *
              * As factors are kept in a list, insertion is O(1).
              *
-             * @param agents The agents the factor returned should be adjacent of.
+             * @param variables The variables the factor returned should be adjacent of.
              *
              * @return An iterator to the desired factor.
              */
-            FactorIt getFactor(const Agents & agents);
+            FactorIt getFactor(const Variables & variables);
 
             /**
              * @brief This function removes a factor from the graph.
@@ -138,40 +138,40 @@ namespace AIToolbox::FactoredMDP {
             void erase(FactorIt it);
 
             /**
-             * @brief This function partially removes an agent from the graph.
+             * @brief This function partially removes an variable from the graph.
              *
              * This function does not actually do much, so it is very
              * important that it is used correctly. No factors are
              * modified, so before calling this function all factors
-             * pointing to this agent should be removed.
+             * pointing to this variable should be removed.
              *
              * This function simply clears the adjacency list for the
-             * specified agent, and decreases the number of agents by one,
-             * so that agentSize() reports one less agent.
+             * specified variable, and decreases the number of variables by one,
+             * so that variableSize() reports one less variable.
              *
              * Calling this function multiple times will continue to
              * decrease the counter! This will have no side effect aside
-             * that the agentSize() function will become meaningless.
+             * that the variableSize() function will become meaningless.
              *
-             * @param agent The agent to be removed.
+             * @param variable The variable to be removed.
              */
-            void erase(size_t agent);
+            void erase(size_t variable);
 
             /**
-             * @brief This function returns the number of agents still in the graph.
+             * @brief This function returns the number of variables still in the graph.
              *
-             * This function basically returns the number of agents this
+             * This function basically returns the number of variables this
              * graph has been initialized with, minus the number of times
              * the erase(size_t) function has been called (even for the
-             * same agent!).
+             * same variable!).
              *
              * This means that if the erase(size_t) function is used
-             * generously, multiple times per agent, this function's return
+             * generously, multiple times per variable, this function's return
              * value is meaningless.
              *
-             * @return The number of agents still in the graph.
+             * @return The number of variables still in the graph.
              */
-            size_t agentSize() const;
+            size_t variableSize() const;
 
             /**
              * @brief This function returns the number of factors still in the graph.
@@ -216,37 +216,37 @@ namespace AIToolbox::FactoredMDP {
 
         private:
             FactorList factorAdjacencies_;
-            std::unordered_map<Agents, FactorIt, boost::hash<Agents>> factorByAgents_;
+            std::unordered_map<Variables, FactorIt, boost::hash<Variables>> factorByVariables_;
 
-            AgentList agentAdjacencies_;
-            size_t activeAgents_;
+            VariableList variableAdjacencies_;
+            size_t activeVariables_;
     };
 
     template <typename Factor>
-    FactorGraph<Factor>::FactorGraph(size_t agents) : agentAdjacencies_(agents), activeAgents_(agents) {}
+    FactorGraph<Factor>::FactorGraph(size_t variables) : variableAdjacencies_(variables), activeVariables_(variables) {}
 
     template <typename Factor>
-    const typename FactorGraph<Factor>::FactorItList & FactorGraph<Factor>::getNeighbors(const size_t agent) const {
-        return agentAdjacencies_[agent].factors_;
+    const typename FactorGraph<Factor>::FactorItList & FactorGraph<Factor>::getNeighbors(const size_t variable) const {
+        return variableAdjacencies_[variable].factors_;
     }
 
     template <typename Factor>
-    const typename FactorGraph<Factor>::Agents & FactorGraph<Factor>::getNeighbors(FactorIt factor) const {
-        return factor->agents_;
+    const typename FactorGraph<Factor>::Variables & FactorGraph<Factor>::getNeighbors(FactorIt factor) const {
+        return factor->variables_;
     }
 
     template <typename Factor>
-    const typename FactorGraph<Factor>::Agents & FactorGraph<Factor>::getNeighbors(CFactorIt factor) const {
-        return factor->agents_;
+    const typename FactorGraph<Factor>::Variables & FactorGraph<Factor>::getNeighbors(CFactorIt factor) const {
+        return factor->variables_;
     }
 
     template <typename Factor>
-    typename FactorGraph<Factor>::Agents FactorGraph<Factor>::getNeighbors(const FactorItList & factors) const {
-        Agents list1, list2;
-        Agents *a1 = &list1, *a2 = &list2;
+    typename FactorGraph<Factor>::Variables FactorGraph<Factor>::getNeighbors(const FactorItList & factors) const {
+        Variables list1, list2;
+        Variables *a1 = &list1, *a2 = &list2;
         for (const auto factor : factors) {
-            const auto & agents = factor->agents_;
-            std::set_union(std::begin(agents), std::end(agents), std::begin(*a1), std::end(*a1), std::back_inserter(*a2));
+            const auto & variables = factor->variables_;
+            std::set_union(std::begin(variables), std::end(variables), std::begin(*a1), std::end(*a1), std::back_inserter(*a2));
             std::swap(a1, a2);
             a2->clear();
         }
@@ -254,41 +254,41 @@ namespace AIToolbox::FactoredMDP {
     }
 
     template <typename Factor>
-    typename FactorGraph<Factor>::FactorIt FactorGraph<Factor>::getFactor(const Agents & agents) {
-        const auto found = factorByAgents_.find(agents);
-        if (found != factorByAgents_.end())
+    typename FactorGraph<Factor>::FactorIt FactorGraph<Factor>::getFactor(const Variables & variables) {
+        const auto found = factorByVariables_.find(variables);
+        if (found != factorByVariables_.end())
             return found->second;
 
         factorAdjacencies_.emplace_back(FactorNode());
         auto it = --factorAdjacencies_.end();
 
-        it->agents_ = agents;
-        for (const auto a : agents)
-            agentAdjacencies_[a].factors_.push_back(it);
+        it->variables_ = variables;
+        for (const auto a : variables)
+            variableAdjacencies_[a].factors_.push_back(it);
 
-        factorByAgents_[agents] = it;
+        factorByVariables_[variables] = it;
         return it;
     }
 
     template <typename Factor>
     void FactorGraph<Factor>::erase(FactorIt it) {
-        for (const auto agent : it->agents_) {
-            auto & factors = agentAdjacencies_[agent].factors_;
+        for (const auto variable : it->variables_) {
+            auto & factors = variableAdjacencies_[variable].factors_;
             const auto foundIt = std::find(std::begin(factors), std::end(factors), it);
             if (foundIt != std::end(factors)) factors.erase(foundIt);
         }
-        factorByAgents_.erase(it->agents_);
+        factorByVariables_.erase(it->variables_);
         factorAdjacencies_.erase(it);
     }
 
     template <typename Factor>
     void FactorGraph<Factor>::erase(const size_t a) {
-        agentAdjacencies_[a].factors_.clear();
-        --activeAgents_;
+        variableAdjacencies_[a].factors_.clear();
+        --activeVariables_;
     }
 
     template <typename Factor>
-    size_t FactorGraph<Factor>::agentSize() const  { return activeAgents_; }
+    size_t FactorGraph<Factor>::variableSize() const  { return activeVariables_; }
     template <typename Factor>
     size_t FactorGraph<Factor>::factorSize() const { return factorAdjacencies_.size(); }
     template <typename Factor>
