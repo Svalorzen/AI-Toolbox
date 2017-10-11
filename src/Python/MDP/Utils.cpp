aa849fe289b35d15f717cba678da1c215e450b76
Svalorzen <svalorzen@gmail.com>
2017-10-11 16:45:06 +0200

    Updated Python docs for utility functions

--- src/Python/MDP/Utils.cpp
+++ src/Python/MDP/Utils.cpp
@@ -6,5 +6,14 @@ void exportMDPUtils() {
     using namespace boost::python;
     using namespace AIToolbox::MDP;
 
-    def("makeQFunction", makeQFunction);
+    def("makeQFunction", makeQFunction,
+        "This function creates an AIToolbox QFunction structure. This can be\n"
+        "directly useful when creating multiple interdependent AIToolbox classes\n"
+        "which also need to share a single QFunction (e.g. ExpectedSARSA and\n"
+        "QGreedyPolicy).\n"
+        "\n"
+        "@param S The size of the state space of the QFunction.\n"
+        "@param A The size of the action space of the QFunction."
+        , (arg("S"), "A")
+    );
 }
--- src/Python/POMDP/Utils.cpp
+++ src/Python/POMDP/Utils.cpp
@@ -14,6 +14,26 @@ void exportPOMDPUtils() {
     using namespace boost::python;
     using namespace AIToolbox::POMDP;
 
-    def("updateBelief", static_cast<Belief (*)(const POMDPModelBinded &, const Belief &, const size_t, const size_t)>(updateBelief));
-    def("updateBelief", static_cast<Belief (*)(const POMDPSparseModelBinded & model, const Belief &, const size_t, const size_t)>(updateBelief));
+    def("updateBelief", static_cast<Belief (*)(const POMDPModelBinded &, const Belief &, const size_t, const size_t)>(updateBelief),
+        "This function creates a new Belief by updating the input Belie\n"
+        "with the input action and observation, following th\n"
+        "transition/observation functions contained in the input model\n"
+        "\n"
+        "@param model The model used to update the belief\n"
+        "@param b The old belief\n"
+        "@param a The action taken during the transition\n"
+        "@param o The observation registered"
+    , (args("model"), "b", "a", "o")
+    );
+    def("updateBelief", static_cast<Belief (*)(const POMDPSparseModelBinded & model, const Belief &, const size_t, const size_t)>(updateBelief),
+        "This function creates a new Belief by updating the input Belie\n"
+        "with the input action and observation, following th\n"
+        "transition/observation functions contained in the input model\n"
+        "\n"
+        "@param model The model used to update the belief\n"
+        "@param b The old belief\n"
+        "@param a The action taken during the transition\n"
+        "@param o The observation registered"
+    , (args("model"), "b", "a", "o")
+    );
 }
