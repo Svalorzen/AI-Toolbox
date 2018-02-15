211ab8bf3164db48dad7050c72de61663acae238
Svalorzen <svalorzen@gmail.com>
2018-02-15 23:21:14 +0100

    Added ability to seed the Seeder

--- src/Impl/Seeder.cpp
+++ src/Impl/Seeder.cpp
@@ -7,9 +7,12 @@ namespace AIToolbox::Impl {
     Seeder::Seeder() : generator_(std::chrono::system_clock::now().time_since_epoch().count()) {}
 
     unsigned Seeder::getSeed() {
-        static Seeder instance;
         static std::uniform_int_distribution<unsigned> dist(0, std::numeric_limits<unsigned>::max());
 
-        return dist(instance.generator_);
+        return dist(instance_.generator_);
+    }
+
+    void Seeder::setRootSeed(const unsigned seed) {
+        instance_.generator_.seed(seed);
     }
 }
