86b58153cb66ddf11af3ece660efd14c2fdad011
Svalorzen <svalorzen@gmail.com>
2018-08-14 17:11:34 +0200

    Temporarily export computeOptimisticValue to Python POMDP

--- src/Python/POMDP/Utils.cpp
+++ src/Python/POMDP/Utils.cpp
@@ -6,6 +6,15 @@
 #include <AIToolbox/POMDP/SparseModel.hpp>
 
 #include <boost/python.hpp>
+#include "../Utils.hpp"
+
+// Temporary until we find a place for these functions
+#include <AIToolbox/POMDP/Algorithms/LinearSupport.hpp>
+
+/// Wrapper for Python of the same named function
+double computeOptimisticValueWrapper(const AIToolbox::Vector & p, const std::vector<std::pair<AIToolbox::Vector, double>> & pvPairs) {
+    return AIToolbox::POMDP::computeOptimisticValue(p, std::begin(pvPairs), std::end(pvPairs));
+}
 
 using POMDPModelBinded = AIToolbox::POMDP::Model<AIToolbox::MDP::Model>;
 using POMDPSparseModelBinded = AIToolbox::POMDP::SparseModel<AIToolbox::MDP::SparseModel>;
@@ -35,5 +44,29 @@ void exportPOMDPUtils() {
         "@param a The action taken during the transition\n"
         "@param o The observation registered"
     , (args("model"), "b", "a", "o")
+    );
+
+    // We'll move the function below in another file at some point.
+    using VVPair = std::pair<AIToolbox::Vector, double>;
+    PairFromPython<VVPair>();
+    VectorFromPython<VVPair>();
+    def("computeOptimisticValue", computeOptimisticValueWrapper,
+         "This function computes the optimistic value of a point given known vertices and values.\n"
+         "\n"
+         "This function computes an LP to determine the best possible value of a\n"
+         "point given all known best vertices around it.\n"
+         "\n"
+         "This function is needed in multi-objective settings (rather than\n"
+         "POMDPs), since the step where we compute the optimal value for a given\n"
+         "point is extremely expensive (it requires solving a full MDP). Thus\n"
+         "linear programming is used in order to determine an optimistic bound\n"
+         "when deciding the next point to extract from the queue during the linear\n"
+         "support process.\n"
+         "\n"
+         "@param p The point where we want to compute the best possible value.\n"
+         "@param pvPairs A list of point-value pairs representing all surrounding vertices.\n"
+         "\n"
+         "@return The best possible value that the input point can have given the known vertices."
+         , (arg("belief"), "bvPairs")
     );
 }
