97aeaccfbb6e8fc30cc426f1c2a998a4df05dfe5
Svalorzen <svalorzen@gmail.com>
2018-09-07 23:53:12 +0200

    Added tests for the new offline methods

---
+++ test/MDP/QLTests.cpp
@@ -0,0 +1,59 @@
+#define BOOST_TEST_MODULE MDP_QL
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <AIToolbox/MDP/SparseModel.hpp>
+#include <AIToolbox/MDP/Algorithms/QL.hpp>
+#include <AIToolbox/MDP/Utils.hpp>
+
+#include <AIToolbox/MDP/Policies/RandomPolicy.hpp>
+#include <AIToolbox/MDP/Policies/EpsilonPolicy.hpp>
+#include <AIToolbox/MDP/Policies/QGreedyPolicy.hpp>
+
+#include "Utils/CliffProblem.hpp"
+
+BOOST_AUTO_TEST_CASE( cliff ) {
+    namespace mdp = AIToolbox::MDP;
+
+    GridWorld grid(12, 3);
+
+    auto model = makeCliffProblem(grid);
+
+    mdp::RandomPolicy behaviour(model.getS(), model.getA());
+    mdp::QL solver(behaviour, 0.1);
+
+    mdp::QGreedyPolicy gPolicy(solver.getQFunction());
+
+    size_t start = model.getS() - 2;
+
+    constexpr auto episodes = 3000;
+    size_t s, a;
+    for ( int episode = 0; episode < episodes; ++episode ) {
+        solver.setExploration(0.9 + (0.1 / episodes) * episode);
+        s = start;
+        for ( int i = 0; i < 10000; ++i ) {
+            a = behaviour.sampleAction( s );
+            const auto [s1, rew] = model.sampleSR( s, a );
+
+            solver.stepUpdateQ( s, a, s1, rew );
+
+            if ( s1 == model.getS() - 1 ) break;
+            s = s1;
+        }
+    }
+
+    // Final path should be: UPx1, RIGHTx11, DOWNx1. Total moves: 13
+    // We can use states only from above the cliff though
+    BOOST_CHECK_EQUAL( gPolicy.getActionProbability(start, UP), 1.0 );
+
+    auto state = grid(0, 2);
+    for ( int i = 0; i < 11; ++i ) {
+        BOOST_CHECK_EQUAL( gPolicy.getActionProbability(state, RIGHT), 1.0 );
+        state.setAdjacent(RIGHT);
+    }
+    for ( int i = 0; i < 1; ++i ) {
+        BOOST_CHECK_EQUAL( gPolicy.getActionProbability(state, DOWN), 1.0 );
+        state.setAdjacent(DOWN);
+    }
+}
