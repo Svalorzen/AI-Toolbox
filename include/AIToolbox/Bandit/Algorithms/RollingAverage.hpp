4ff796a04b2c8367f2a20da0b171c588dd0338db
Svalorzen <svalorzen@gmail.com>
2018-12-17 18:53:57 +0100

    Added docs to RollingAverage

--- include/AIToolbox/Bandit/Algorithms/RollingAverage.hpp
+++ include/AIToolbox/Bandit/Algorithms/RollingAverage.hpp
@@ -4,15 +4,48 @@
 #include <AIToolbox/Bandit/Types.hpp>
 
 namespace AIToolbox::Bandit {
+    /**
+     * @brief This class computes averages and counts for a Bandit problem.
+     *
+     * This class can be used to compute the averages and counts for all
+     * actions in a Bandit problem over time.
+     */
     class RollingAverage {
         public:
+            /**
+             * @brief Basic constructor.
+             *
+             * @param A The size of the action space.
+             */
             RollingAverage(size_t A);
 
+            /**
+             * @brief This function updates the QFunction and counts.
+             *
+             * @param a The action taken.
+             * @param rew The reward obtained.
+             */
             void stepUpdateQ(size_t a, double rew);
 
+            /**
+             * @brief This function returns the size of the action space.
+             *
+             * @return The size of the action space.
+             */
             size_t getA() const;
 
+            /**
+             * @brief This function returns a reference to the internal QFunction.
+             *
+             * @return A reference to the internal QFunction.
+             */
             const QFunction & getQFunction() const;
+
+            /**
+             * @brief This function returns a reference for the counts for the actions.
+             *
+             * @return A reference to the counts of the actions.
+             */
             const std::vector<unsigned> & getCounts() const;
 
         private:
