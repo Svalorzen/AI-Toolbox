5996d13409f578998a0bcb9b56527094c0bf111f
Svalorzen <svalorzen@gmail.com>
2019-01-26 11:48:29 +0100

    Added initial test file for new method

---
+++ test/Factored/LinearProgrammingTests.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE Factored_MDP_LinearProgramming
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <AIToolbox/Factored/MDP/Algorithms/LinearProgramming.hpp>
+
+BOOST_AUTO_TEST_CASE( solver ) {
+}
