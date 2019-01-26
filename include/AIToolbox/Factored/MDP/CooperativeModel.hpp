6eabfd04a8e3d10025839760e4ea1f3ac5edfc98
Svalorzen <svalorzen@gmail.com>
2019-01-26 23:36:13 +0100

    Added some missing functions to CooperativeModel

--- include/AIToolbox/Factored/MDP/CooperativeModel.hpp
+++ include/AIToolbox/Factored/MDP/CooperativeModel.hpp
@@ -24,8 +24,29 @@ namespace AIToolbox::Factored::MDP {
              * @param a The action space.
              * @param transitions The transition function.
              * @param rewards The reward function.
+             * @param discount The discount factor for the MDP.
              */
-            CooperativeModel(State s, Action a, FactoredDDN transitions, Factored2DMatrix rewards);
+            CooperativeModel(State s, Action a, FactoredDDN transitions, Factored2DMatrix rewards, double discount = 1.0);
+
+            /**
+             * @brief This function samples the MDP with the specified state action pair.
+             *
+             * This function samples the model for simulated experience.
+             * The transition and reward functions are used to produce,
+             * from the state action pair inserted as arguments, a possible
+             * new state with respective reward. The new state is picked
+             * from all possible states that the MDP allows transitioning
+             * to, each with probability equal to the same probability of
+             * the transition in the model. After a new state is picked,
+             * the reward is the corresponding reward contained in the
+             * reward function.
+             *
+             * @param s The state that needs to be sampled.
+             * @param a The action that needs to be sampled.
+             *
+             * @return A tuple containing a new state and a reward.
+             */
+            std::tuple<State, double> sampleSR(const State & s, const Action & a) const;
 
             /**
              * @brief This function sets a new discount factor for the Model.
@@ -56,11 +77,39 @@ namespace AIToolbox::Factored::MDP {
             double getDiscount() const;
 
             /**
+             * @brief This function returns the stored transition probability for the specified transition.
+             *
+             * @param s The initial state of the transition.
+             * @param a The action performed in the transition.
+             * @param s1 The final state of the transition.
+             *
+             * @return The probability of the specified transition.
+             */
+            double getTransitionProbability(const State & s, const Action & a, const State & s1) const;
+
+            /**
+             * @brief This function returns the stored expected reward for the specified transition.
+             *
+             * @param s The initial state of the transition.
+             * @param a The action performed in the transition.
+             * @param s1 The final state of the transition.
+             *
+             * @return The expected reward of the specified transition.
+             */
+            double getExpectedReward(const State & s, const Action & a, const State & s1) const;
+
+            /**
              * @brief This function returns the transition function of the MDP.
              *
              * @return The transition function of the MDP.
              */
             const FactoredDDN & getTransitionFunction() const;
+
+            /**
+             * @brief This function
+             *
+             * @return
+             */
             const Factored2DMatrix & getRewardFunction() const;
         private:
             State S;
@@ -69,6 +118,8 @@ namespace AIToolbox::Factored::MDP {
 
             FactoredDDN transitions_;
             Factored2DMatrix rewards_;
+
+            mutable RandomEngine rand_;
     };
 }
 
--- src/Factored/MDP/CooperativeModel.cpp
+++ src/Factored/MDP/CooperativeModel.cpp
@@ -1,9 +1,43 @@
 #include <AIToolbox/Factored/MDP/CooperativeModel.hpp>
 
+#include <AIToolbox/Impl/Seeder.hpp>
+#include <AIToolbox/Utils/Probability.hpp>
+#include <AIToolbox/Factored/Utils/Core.hpp>
+
 namespace AIToolbox::Factored::MDP {
+    CooperativeModel::CooperativeModel(State s, Action a, FactoredDDN transitions, Factored2DMatrix rewards, const double discount) :
+            S(std::move(s)), A(std::move(a)), discount_(discount),
+            transitions_(std::move(transitions)), rewards_(std::move(rewards)),
+            rand_(Impl::Seeder::getSeed()) {}
+
+    std::tuple<State, double> CooperativeModel::sampleSR(const State & s, const Action & a) const {
+        State s1(S.size());
+
+        for (size_t i = 0; i < S.size(); ++i) {
+            const auto actionId = toIndexPartial(transitions_[i].actionTag, A, a);
+
+            const auto & node = transitions_[i].nodes[actionId];
+            const auto parentId = toIndexPartial(node.tag, S, s);
+
+            const size_t newS = sampleProbability(S[i], node.matrix.row(parentId), rand_);
+
+            s1[i] = newS;
+        }
+
+        return std::make_tuple(s1, rewards_.getValue(S, A, s, a));
+    }
+    double CooperativeModel::getTransitionProbability(const State & s, const Action & a, const State & s1) const {
+        return transitions_.getTransitionProbability(S, A, s, a, s1);
+    }
+
+    double CooperativeModel::getExpectedReward(const State & s, const Action & a, const State &) const {
+        return rewards_.getValue(S, A, s, a);
+    }
+
     const State & CooperativeModel::getS() const { return S; }
     const Action & CooperativeModel::getA() const { return A; }
     double CooperativeModel::getDiscount() const { return discount_; }
     const FactoredDDN & CooperativeModel::getTransitionFunction() const { return transitions_; }
     const Factored2DMatrix & CooperativeModel::getRewardFunction() const { return rewards_; }
 }
+
