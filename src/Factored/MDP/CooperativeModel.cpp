a574b2d016e80a5b0be7b164a8e07db9d0c1e5e1
Svalorzen <svalorzen@gmail.com>
2019-01-29 14:14:03 +0100

    Added validation for CooperativeModel

--- src/Factored/MDP/CooperativeModel.cpp
+++ src/Factored/MDP/CooperativeModel.cpp
@@ -5,10 +5,138 @@
 #include <AIToolbox/Factored/Utils/Core.hpp>
 
 namespace AIToolbox::Factored::MDP {
+    enum class TagErrors {
+        None,
+        NoElements,
+        TooManyElements,
+        IdTooHigh,
+        NotSorted,
+        Duplicates,
+    };
+
+    std::pair<TagErrors, size_t> checkTag(const Factors & space, const PartialKeys & tag) {
+        // Check action tag size.
+        if (tag.size() == 0)
+            return std::make_pair(TagErrors::NoElements, 0);
+        if (tag.size() > space.size())
+            return std::make_pair(TagErrors::TooManyElements, 0);
+
+        // Check action tag elements for ordering and size.
+        auto previousV = tag[0];
+        if (previousV >= space.size())
+            return std::make_pair(TagErrors::IdTooHigh, 0);
+
+        for (size_t t = 1; t < tag.size(); ++t) {
+            const auto tagV = tag[t];
+            if (tagV >= space.size()) return std::make_pair(TagErrors::IdTooHigh, t);
+            if (tagV <  previousV)    return std::make_pair(TagErrors::NotSorted, t);
+            if (tagV == previousV)    return std::make_pair(TagErrors::Duplicates, t);
+            previousV = tagV;
+        }
+        return std::make_pair(TagErrors::None, 0);
+    }
+
     CooperativeModel::CooperativeModel(State s, Action a, FactoredDDN transitions, Factored2DMatrix rewards, const double discount) :
             S(std::move(s)), A(std::move(a)), discount_(discount),
             transitions_(std::move(transitions)), rewards_(std::move(rewards)),
-            rand_(Impl::Seeder::getSeed()) {}
+            rand_(Impl::Seeder::getSeed())
+    {
+        // Now we validate both the transition function and the rewards.
+        if (transitions_.nodes.size() != S.size())
+            throw std::invalid_argument("Input DDN has an incorrect number of nodes!");
+
+        for (size_t s = 0; s < S.size(); ++s) {
+            const auto & node = transitions_.nodes[s];
+
+            auto [error, id] = checkTag(A, node.actionTag);
+            switch (error) {
+                case TagErrors::NoElements:
+                    throw std::invalid_argument("Input DDN contains action tags with no elements!");
+                case TagErrors::TooManyElements:
+                    throw std::invalid_argument("Input DDN contains action tags with too many elements!");
+                case TagErrors::IdTooHigh:
+                    throw std::invalid_argument("Input DDN references action IDs too high for the action space!");
+                case TagErrors::NotSorted:
+                    throw std::invalid_argument("Input DDN contains action tags that are not sorted!");
+                case TagErrors::Duplicates:
+                    throw std::invalid_argument("Input DDN contains duplicate action tags entries!");
+                default:;
+            }
+
+            // Check number of nodes is correct.
+            if (node.nodes.size() != factorSpacePartial(node.actionTag, A))
+                throw std::invalid_argument("Input DDN has an incorrect number of bayesian nodes for the specified action tag!");
+
+            for (size_t a = 0; a < node.nodes.size(); ++a) {
+                const auto & subnode = node.nodes[a];
+
+                auto [error, id] = checkTag(S, subnode.tag);
+                switch (error) {
+                    case TagErrors::NoElements:
+                        throw std::invalid_argument("Input DDN contains subnode tags with no elements!");
+                    case TagErrors::TooManyElements:
+                        throw std::invalid_argument("Input DDN contains subnode tags with too many elements!");
+                    case TagErrors::IdTooHigh:
+                        throw std::invalid_argument("Input DDN references state IDs too high for the state space!");
+                    case TagErrors::NotSorted:
+                        throw std::invalid_argument("Input DDN contains state tags that are not sorted!");
+                    case TagErrors::Duplicates:
+                        throw std::invalid_argument("Input DDN contains duplicate state tags entries!");
+                    default:;
+                }
+
+                // Check size of matrix is correct
+                if (subnode.matrix.cols() != static_cast<long>(S[s]))
+                    throw std::invalid_argument("Input DDN contains matrices with incorrect number of columns!");
+
+                if (subnode.matrix.rows() != static_cast<long>(factorSpacePartial(subnode.tag, S)))
+                    throw std::invalid_argument("Input DDN contains matrices with incorrect number of rows!");
+
+                // Check each row is a probability.
+                for (int i = 0; i < subnode.matrix.rows(); ++i)
+                    if (!isProbability(S[s], subnode.matrix.row(i)))
+                        throw std::invalid_argument("Input DDN contains invalid probabilities!");
+            }
+        }
+
+        for (const auto & r : rewards_.bases) {
+            auto [error, id] = checkTag(A, r.actionTag);
+            switch (error) {
+                case TagErrors::NoElements:
+                    throw std::invalid_argument("Input reward function contains action tags with no elements!");
+                case TagErrors::TooManyElements:
+                    throw std::invalid_argument("Input reward function contains action tags with too many elements!");
+                case TagErrors::IdTooHigh:
+                    throw std::invalid_argument("Input reward function references action IDs too high for the action space!");
+                case TagErrors::NotSorted:
+                    throw std::invalid_argument("Input reward function contains action tags that are not sorted!");
+                case TagErrors::Duplicates:
+                    throw std::invalid_argument("Input reward function contains duplicate action tags entries!");
+                default:;
+            }
+            std::tie(error, id) = checkTag(S, r.tag);
+            switch (error) {
+                case TagErrors::NoElements:
+                    throw std::invalid_argument("Input reward function contains subnode tags with no elements!");
+                case TagErrors::TooManyElements:
+                    throw std::invalid_argument("Input reward function contains subnode tags with too many elements!");
+                case TagErrors::IdTooHigh:
+                    throw std::invalid_argument("Input reward function references state IDs too high for the state space!");
+                case TagErrors::NotSorted:
+                    throw std::invalid_argument("Input reward function contains state tags that are not sorted!");
+                case TagErrors::Duplicates:
+                    throw std::invalid_argument("Input reward function contains duplicate state tags entries!");
+                default:;
+            }
+
+            // Check size of matrix is correct
+            if (r.values.cols() != static_cast<long>(factorSpacePartial(r.actionTag, A)))
+                throw std::invalid_argument("Input reward function contains bases with incorrect number of columns!");
+
+            if (r.values.rows() != static_cast<long>(factorSpacePartial(r.tag, S)))
+                throw std::invalid_argument("Input reward function contains bases with incorrect number of rows!");
+        }
+    }
 
     std::tuple<State, double> CooperativeModel::sampleSR(const State & s, const Action & a) const {
         State s1(S.size());
