e7287435717fdbc2a0772670225289941d4a87d2
Svalorzen <svalorzen@gmail.com>
2019-03-05 21:20:32 +0100

    Updated MOVE to new GVE

--- src/Factored/Bandit/Algorithms/Utils/MultiObjectiveVariableElimination.cpp
+++ src/Factored/Bandit/Algorithms/Utils/MultiObjectiveVariableElimination.cpp
@@ -10,6 +10,40 @@
 namespace AIToolbox::Factored::Bandit {
     using MOVE = MultiObjectiveVariableElimination;
 
+    namespace {
+        struct Global {
+            const Action & A;
+            MOVE::Results results;
+
+            size_t agent;
+            size_t agentAction;
+            MOVE::Factor newFactor;
+            MOVE::Factor newCrossSum;
+            MOVE::Factor newFactorCrossSum;
+
+            void beginRemoval(size_t agent);
+            //void beginRemoval(const MOVE::GVE::Graph &, const MOVE::GVE::Graph::FactorItList &, const MOVE::GVE::Graph::Variables &, size_t agent);
+            void initNewFactor();
+            void beginCrossSum(size_t agentAction);
+            void beginFactorCrossSum();
+            void crossSum(const MOVE::Factor & f);
+            void endFactorCrossSum();
+            void endCrossSum();
+            bool isValidNewFactor();
+            MOVE::GVE::Rules mergeRules(MOVE::GVE::Rules && lhs, MOVE::GVE::Rules && rhs);
+            void makeResult(MOVE::GVE::FinalFactors && finalFactors);
+        };
+    }
+
+    MOVE::Results MOVE::operator()(const Action & A, GVE::Graph & graph) {
+        GVE gve;
+        Global global{A, {}, 0, 0, {}, {}, {}};
+
+        gve(A, graph, global);
+
+        return global.results;
+    }
+
     /**
      * @brief This function cross-sums the input lists.
      *
@@ -22,177 +56,127 @@ namespace AIToolbox::Factored::Bandit {
      *
      * @return A new list containing all cross-sums.
      */
-    MOVE::Entries crossSum(const MOVE::Entries & lhs, const MOVE::Entries & rhs);
-
-    /**
-     * @brief This function cross-sums the input lists.
-     *
-     * Cross-sums are performed considering the right-hand side as one
-     * single joined list. This is useful considering how the getPayoffs()
-     * function works.
-     *
-     * \sa crossSum(const MOVE::Entries &, const MOVE::Entries &);
-     *
-     * @param lhs The left hand side.
-     * @param rhs A list of pointers to valid Entries lists.
-     *
-     * @return A new list containing all cross-sums.
-     */
-    MOVE::Entries crossSum(const MOVE::Entries & lhs, const std::vector<const MOVE::Entries*> rhs);
-
-    /**
-     * @brief This function returns a list of pointers to all Entries from the Rules matching the input joint action.
-     *
-     * @param keys The keys of the agents of the rules.
-     * @param rules A list of Rule.
-     * @param jointAction A joint action to match Rules against.
-     *
-     * @return A list of pointers to the Entries contained in the Rules matched against the input action.
-     */
-    std::vector<const MOVE::Entries*> getPayoffs(const PartialKeys & keys, const MOVE::Rules & rules, const PartialAction & jointAction);
-
-    /**
-     * @brief This function returns cross-sums common elements between the input plus all unique Rules.
-     *
-     * The inputs must be sorted by PartialAction lexically. This function
-     * moves from its inputs.
-     *
-     * @param lhs The left hand side.
-     * @param rhs The right hand side.
-     *
-     * @return A list of cross-summed rules.
-     */
-    MOVE::Rules mergePayoffs(MOVE::Rules && lhs, MOVE::Rules && rhs);
-
-    // -----------------------
-
-    MOVE::MultiObjectiveVariableElimination(Action a) : A(std::move(a)), graph_(A.size()) {}
-
-    MOVE::Results MOVE::start() {
-        // This can possibly be improved with some heuristic ordering
-        while (graph_.variableSize())
-            removeAgent(graph_.variableSize() - 1);
-
-        Results retval;
-        if (finalFactors_.size() == 0) return retval;
-
-        for (const auto & fValue : finalFactors_)
-            retval = crossSum(retval, fValue);
-
-        // P1 pruning
-        const auto unwrap = +[](Entry & e) -> Rewards & {return std::get<1>(e);};
-        const auto rbegin = boost::make_transform_iterator(std::begin(retval), unwrap);
-        const auto rend   = boost::make_transform_iterator(std::end  (retval), unwrap);
-
-        retval.erase(AIToolbox::extractDominated(unwrap(retval[0]).size(), rbegin, rend).base(), std::end(retval));
-
+    MOVE::Factor crossSumF(const MOVE::Factor & lhs, const MOVE::Factor & rhs) {
+        if (!lhs.size()) return rhs;
+        if (!rhs.size()) return lhs;
+        MOVE::Factor retval;
+        retval.reserve(lhs.size() * rhs.size());
+        // We do the rhs last since they'll usually be shorter (due to
+        // this class usage), so hopefully we can use the cache better.
+        for (const auto & lhsVal : lhs) {
+            for (const auto & rhsVal : rhs) {
+                auto tags = merge(std::get<0>(lhsVal), std::get<0>(rhsVal));
+                auto values = std::get<1>(lhsVal) + std::get<1>(rhsVal);
+                retval.emplace_back(std::move(tags), std::move(values));
+            }
+        }
         return retval;
     }
 
-    void MOVE::removeAgent(const size_t agent) {
-        const auto factors = graph_.getNeighbors(agent);
-        auto agents = graph_.getNeighbors(factors);
+    void Global::beginRemoval(size_t currAgent) {
+        // We save the currently eliminated agent to initialize the crossSum
+        // tag correctly later.
+        agent = currAgent;
+    }
 
-        Rules newRules;
-        PartialFactorsEnumerator jointActions(A, agents, agent);
-        const auto id = jointActions.getFactorToSkipId();
+    void Global::initNewFactor() {
+        newFactor.clear();
+    }
 
-        const bool isFinalFactor = agents.size() == 1;
+    void Global::beginCrossSum(size_t currAction) {
+        newCrossSum.clear();
 
-        while (jointActions.isValid()) {
-            auto & jointAction = *jointActions;
+        agentAction = currAction;
+    }
 
-            Entries values;
-            for (size_t agentAction = 0; agentAction < A[agent]; ++agentAction) {
-                jointAction.second[id] = agentAction;
+    void Global::beginFactorCrossSum() {
+        // Since we need to compute cross-sums, each factor must be
+        // cross-summed separately. This variable is thus the output of
+        // cross-summing the next factor with the rules we already have in
+        // newCrossSum.
+        newFactorCrossSum.clear();
+    }
 
-                Entries newEntries;
-                // So the idea here is that we are computing results for
-                // this particular subset of agents. Here we are working
-                // with a single action. However, we may have eliminated
-                // agents already. This means that this factor will contain
-                // a certain number of rules, which depend on different
-                // "already taken" actions of the eliminated agents.
-                //
-                // During normal VE, we can simply add up all tags since
-                // they can't possibly conflict (due to the max operator
-                // which always makes us pick the best one). Here instead,
-                // payoffs returned by the getPayoffs function can't get
-                // squashed into a single one and summed, since their tags
-                // are no longer guaranteed unique.
-                //
-                // Thus we get them all, and during the cross/sum we create
-                // even more rules, joining their tags together, and
-                // possibly merge them if we see equal ones.
-                for (size_t i = 0; i < factors.size(); ++i) {
-                    newEntries = crossSum(newEntries, getPayoffs(factors[i]->getVariables(), factors[i]->getData().rules, jointAction));
-                    // p3.prune(&newEntries);
-                }
+    void Global::crossSum(const MOVE::Factor & factor) {
+        // So the idea here is that we are computing results for
+        // this particular subset of agents. Here we are working
+        // with a single action. However, we may have eliminated
+        // agents already. This means that this factor will contain
+        // a certain number of rules, which depend on different
+        // "already taken" actions of the eliminated agents.
+        //
+        // During normal VE, we can simply add up all tags since
+        // they can't possibly conflict (due to the max operator
+        // which always makes us pick the best one). Here instead,
+        // payoffs returned by the getPayoffs function can't get
+        // squashed into a single one and summed, since their tags
+        // are no longer guaranteed unique.
+        //
+        // Thus we get them all, and during the cross/sum we create
+        // even more rules, joining their tags together, and
+        // possibly merge them if we see equal ones.
+        auto tmp = crossSumF(newCrossSum, factor);
 
-                if (newEntries.size() != 0) {
-                    // Add tags
-                    for (auto & nv : newEntries) {
-                        auto & first  = std::get<0>(nv).first;
-                        auto & second = std::get<0>(nv).second;
+        newFactorCrossSum.insert(
+            std::end(newFactorCrossSum),
+            std::make_move_iterator(std::begin(tmp)),
+            std::make_move_iterator(std::end(tmp))
+        );
+    }
 
-                        size_t i = 0;
-                        while (i < first.size() && first[i] < agent) ++i;
-
-                        first.insert(std::begin(first) + i, agent);
-                        second.insert(std::begin(second) + i, agentAction);
-                    }
-                    values.insert(std::end(values), std::make_move_iterator(std::begin(newEntries)),
-                                                    std::make_move_iterator(std::end(newEntries)));
-                }
-            }
-
-            // p2.prune(&values);
-
-            if (values.size() != 0) {
-                // If this is a final factor we do the alternative path
-                // here, to avoid copying joint actions which we won't
-                // really need anymore.
-                if (!isFinalFactor) {
-                    newRules.emplace_back(PartialValues(), std::move(values));
-                    newRules.back().first.reserve(agents.size() - 1);
-                    for (size_t a = 0; a < agents.size(); ++a)
-                        if (a != id) newRules.back().first.push_back(jointAction.second[a]);
-                } else {
-                    finalFactors_.emplace_back(std::move(values));
-                }
-            }
-            jointActions.advance();
-        }
-
-        for (auto & it : factors)
-            graph_.erase(it);
-        graph_.erase(agent);
-
-        if (newRules.size() == 0) return;
-        if (!isFinalFactor) {
-            agents.erase(std::remove(std::begin(agents), std::end(agents), agent), std::end(agents));
-
-            auto newFactor = graph_.getFactor(agents);
-
-            // Unfortunately here we cannot simply dump the new results in
-            // the old factor as we do in the normal VariableElimination.
-            // This is because in VariableElimination all elements are
-            // summed together, which means that it doesn't matter whether
-            // they are grouped or not. Here elements are CROSS-summed,
-            // which means we cannot simply dump stuff lest losing a
-            // cross-summing step.
-            newFactor->getData().rules = mergePayoffs(std::move(newFactor->getData().rules), std::move(newRules));
+    void Global::endFactorCrossSum() {
+        // Here we move back the output of cross-summing the last factor into
+        // our actual set of rules, ready to cross-sum the next.
+        //
+        // Note that in case no factors were found to cross-sum, we don't want
+        // to discard our old results, so we only move if we have something!
+        if (newFactorCrossSum.size() > 0) {
+            // p3 pruning if needed
+            newCrossSum = std::move(newFactorCrossSum);
         }
     }
 
-    bool ruleComp(const MOVE::Rule & lhs, const MOVE::Rule & rhs) {
-        return veccmp(lhs.first, rhs.first) < 0;
+    void Global::endCrossSum() {
+        if (newCrossSum.size() > 0) {
+            // Add tags
+            for (auto & nv : newCrossSum) {
+                auto & [first, second] = std::get<0>(nv);
+
+                size_t i = 0;
+                while (i < first.size() && first[i] < agent) ++i;
+
+                first.insert(std::begin(first) + i, agent);
+                second.insert(std::begin(second) + i, agentAction);
+            }
+            newFactor.insert(
+                std::end(newFactor),
+                std::make_move_iterator(std::begin(newCrossSum)),
+                std::make_move_iterator(std::end(newCrossSum))
+            );
+        }
     }
 
-    MOVE::Rules mergePayoffs(MOVE::Rules && lhs, MOVE::Rules && rhs) {
-        MOVE::Rules retval;
+    bool Global::isValidNewFactor() {
+        // p2.prune(&newFactor);
+
+        return newFactor.size() > 0;
+    }
+
+    MOVE::GVE::Rules Global::mergeRules(MOVE::GVE::Rules && lhs, MOVE::GVE::Rules && rhs) {
+        // Unfortunately here we cannot simply dump the new results in
+        // the old factor as we do in the normal VariableElimination.
+        // This is because in VariableElimination all elements are
+        // summed together, which means that it doesn't matter whether
+        // they are grouped or not. Here elements are CROSS-summed,
+        // which means we cannot simply dump stuff lest losing a
+        // cross-summing step.
+        MOVE::GVE::Rules retval;
         // We're going to have at least these rules.
         retval.reserve(lhs.size() + rhs.size());
+
+        auto ruleComp = [](const auto & lhs, const auto & rhs) {
+            return veccmp(lhs.first, rhs.first) < 0;
+        };
 
         std::sort(std::begin(lhs), std::end(lhs), ruleComp);
         std::sort(std::begin(rhs), std::end(rhs), ruleComp);
@@ -208,7 +192,7 @@ namespace AIToolbox::Factored::Bandit {
             else if (first > 0)
                 retval.emplace_back(std::move(rhs[j++]));
             else {
-                retval.emplace_back(lhs[i].first, crossSum(lhs[i].second, rhs[j].second));
+                retval.emplace_back(lhs[i].first, crossSumF(lhs[i].second, rhs[j].second));
                 ++i; ++j;
             }
         }
@@ -221,45 +205,17 @@ namespace AIToolbox::Factored::Bandit {
         return retval;
     }
 
+    void Global::makeResult(MOVE::GVE::FinalFactors && finalFactors) {
+        if (finalFactors.size() == 0) return;
 
-    std::vector<const MOVE::Entries*> getPayoffs(const PartialKeys & keys, const MOVE::Rules & rules, const PartialAction & jointAction) {
-        std::vector<const MOVE::Entries*> retval;
-        // Note here that we must use match since the factors adjacent to
-        // one agent aren't all next to all its neighbors. Since they are
-        // different, we must coarsely check that equal agents do equal
-        // actions.
-        for (const auto & rule : rules)
-            if (match(keys, rule.first, jointAction.first, jointAction.second))
-                retval.push_back(&rule.second);
-        return retval;
-    }
+        for (const auto & fValue : finalFactors)
+            results = crossSumF(results, fValue);
 
-    MOVE::Entries crossSum(const MOVE::Entries & lhs, const std::vector<const MOVE::Entries*> rhs) {
-        if (!rhs.size()) return lhs;
+        // P1 pruning
+        const auto unwrap = +[](MOVE::Entry & e) -> Rewards & {return std::get<1>(e);};
+        const auto rbegin = boost::make_transform_iterator(std::begin(results), unwrap);
+        const auto rend   = boost::make_transform_iterator(std::end  (results), unwrap);
 
-        MOVE::Entries retval;
-        for (auto p : rhs) {
-            auto tmp = crossSum(lhs, *p);
-            retval.insert(std::end(retval), std::make_move_iterator(std::begin(tmp)),
-                                            std::make_move_iterator(std::end(tmp)));
-        }
-        return retval;
-    }
-
-    MOVE::Entries crossSum(const MOVE::Entries & lhs, const MOVE::Entries & rhs) {
-        if (!lhs.size()) return rhs;
-        if (!rhs.size()) return lhs;
-        MOVE::Entries retval;
-        retval.reserve(lhs.size() + rhs.size());
-        // We do the rhs last since they'll usually be shorter (due to
-        // this class usage), so hopefully we can use the cache better.
-        for (const auto & lhsVal : lhs) {
-            for (const auto & rhsVal : rhs) {
-                auto tags = merge(std::get<0>(lhsVal), std::get<0>(rhsVal));
-                auto values = std::get<1>(lhsVal) + std::get<1>(rhsVal);
-                retval.emplace_back(std::move(tags), std::move(values));
-            }
-        }
-        return retval;
+        results.erase(AIToolbox::extractDominated(unwrap(results[0]).size(), rbegin, rend).base(), std::end(results));
     }
 }
