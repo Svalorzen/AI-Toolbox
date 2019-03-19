01ba4bc71f25dc2169de8b4997f2bc5607cf9372
Svalorzen <svalorzen@gmail.com>
2019-03-20 00:04:40 +0100

    Added CooperativeRLModel to CPS test

--- test/Factored/CooperativePrioritizedSweepingTests.cpp
+++ test/Factored/CooperativePrioritizedSweepingTests.cpp
@@ -5,9 +5,30 @@
 
 #include <AIToolbox/Utils/Core.hpp>
 #include <AIToolbox/Factored/MDP/Algorithms/CooperativePrioritizedSweeping.hpp>
+#include <AIToolbox/Factored/MDP/CooperativeExperience.hpp>
+#include <AIToolbox/Factored/MDP/CooperativeRLModel.hpp>
+
+#include "Utils/SysAdmin.hpp"
 
 namespace aif = AIToolbox::Factored;
 namespace fm = AIToolbox::Factored::MDP;
 
 BOOST_AUTO_TEST_CASE( simple_rule_update ) {
+    auto problem = makeSysAdminUniRing(2, 0.1, 0.2, 0.3, 0.4, 0.4, 0.4, 0.3);
+
+    fm::CooperativeExperience exp(problem.getS(), problem.getA(), problem.getTransitionFunction().nodes);
+    fm::CooperativeRLModel model(exp, 0.9);
+
+    std::vector<std::vector<size_t>> domains{
+        {0, 1},
+        {2, 3}
+    };
+
+    fm::CooperativePrioritizedSweeping ps(model, domains);
+
+    aif::State s(model.getS());
+    aif::Action a(model.getA());
+    auto [s1, r] = model.sampleSRs(s, a);
+
+    ps.stepUpdateQ(s, a, s1, r);
 }
