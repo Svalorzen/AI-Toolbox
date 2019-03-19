95fcaaeee74630aaa55479289f8285688c651571
Svalorzen <svalorzen@gmail.com>
2019-03-19 17:36:34 +0100

    Added initial file for CPS tests

---
+++ test/Factored/CooperativePrioritizedSweepingTests.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE Factored_MDP_CooperativePrioritizedSweeping
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <AIToolbox/Utils/Core.hpp>
+#include <AIToolbox/Factored/MDP/Algorithms/CooperativePrioritizedSweeping.hpp>
+
+namespace aif = AIToolbox::Factored;
+namespace fm = AIToolbox::Factored::MDP;
+
+BOOST_AUTO_TEST_CASE( simple_rule_update ) {
+}
