07fedbeeaa6b182f5c93d925c71a6cc8452d73a7
Svalorzen <svalorzen@gmail.com>
2019-03-21 15:34:54 +0100

    Refined GridWorld

--- include/AIToolbox/MDP/Environments/Utils/GridWorld.hpp
+++ include/AIToolbox/MDP/Environments/Utils/GridWorld.hpp
@@ -1,65 +1,78 @@
 #ifndef AI_TOOLBOX_MDP_GRIDWORLD_STATE
 #define AI_TOOLBOX_MDP_GRIDWORLD_STATE
 
-#include <algorithm>
+#include <cstddef>
 
-enum Direction { UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3 };
+namespace AIToolbox::MDP {
+    /**
+     * @brief This class represents a simple gridworld.
+     */
+    class GridWorld {
+        public:
+            enum Direction { UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3 };
+            struct State {
+                operator size_t();
+                private:
+                    State(int xx, int yy, size_t ss);
+                    int x, y;
+                    size_t s;
+                    friend GridWorld;
+            };
 
-class GridWorldState {
-    public:
+            /**
+             * @brief Basic constructor.
+             *
+             * @param width The number of columns in the world.
+             * @param height The number of rows in the world.
+             */
+            GridWorld(unsigned width, unsigned height);
 
-        GridWorldState(int sx, int sy, int x, int y) : MAP_SIZE_X(sx), MAP_SIZE_Y(sy) { setX(x); setY(y); }
-        GridWorldState(int sx, int sy, size_t s) : MAP_SIZE_X(sx), MAP_SIZE_Y(sy),
-                                                   x_(std::min((int)s%MAP_SIZE_X, MAP_SIZE_X-1)),
-                                                   y_(std::min((int)s/MAP_SIZE_X, MAP_SIZE_Y-1)) {}
+            /**
+             * @brief This function returns the state next to the input in the chosen Direction.
+             *
+             * This function returns the same state if the Direction goes
+             * outside the GridWorld boundaries.
+             *
+             * @param d The Direction to look for.
+             * @param s The initial State.
+             *
+             * @return The State next to the input.
+             */
+            State getAdjacent(Direction d, State s);
 
-        operator size_t() { return x_ + y_*MAP_SIZE_X; }
+            /**
+             * @brief This function returns the State at the selected position.
+             *
+             * Coordinates are bound to the size of the GridWorld.
+             *
+             * @param x The x of the output State.
+             * @param y The y of the output State.
+             */
+            State operator()(int x, int y) const;
 
-        void setAdjacent(Direction d) {
-            switch ( d ) {
-                case UP:    setY(y_-1); return;
-                case DOWN:  setY(y_+1); return;
-                case LEFT:  setX(x_-1); return;
-                case RIGHT: setX(x_+1); return;
-            }
-        }
-        void setX(int newX) {
-            if ( newX < 0 ) x_ = 0;
-            else if ( newX >= MAP_SIZE_X ) x_ = MAP_SIZE_X - 1;
-            else x_ = newX;
-        }
-        void setY(int newY) {
-            if ( newY < 0 ) y_ = 0;
-            else if ( newY >= MAP_SIZE_Y ) y_ = MAP_SIZE_Y - 1;
-            else y_ = newY;
-        }
-        int getX() const { return x_; }
-        int getY() const { return y_; }
+            /**
+             * @brief This function returns the State with the input numerical representation.
+             *
+             * @param s The State numerical representation.
+             */
+            State operator()(const size_t s) const;
 
-    private:
-        int MAP_SIZE_X, MAP_SIZE_Y;
+            /**
+             * @brief This function returns the width of the GridWorld.
+             */
+            unsigned getWidth() const;
 
-        int x_, y_;
-};
+            /**
+             * @brief This function returns the height of the GridWorld.
+             */
+            unsigned getHeight() const;
 
-class GridWorld {
-    public:
-        GridWorld(size_t x, size_t y) : MAP_SIZE_X(x), MAP_SIZE_Y(y) {}
+        private:
+            int boundX(int x) const;
+            int boundY(int y) const;
 
-        GridWorldState operator()(size_t x, size_t y) const {
-            return GridWorldState(MAP_SIZE_X, MAP_SIZE_Y, x, y);
-        }
-
-        GridWorldState operator()(size_t s) const {
-            return GridWorldState(MAP_SIZE_X, MAP_SIZE_Y, s);
-        }
-
-        size_t getSizeX() const { return MAP_SIZE_X; }
-        size_t getSizeY() const { return MAP_SIZE_Y; }
-
-    private:
-        int MAP_SIZE_X, MAP_SIZE_Y;
-};
-
+            unsigned width_, height_;
+    };
+}
 
 #endif
---
+++ src/MDP/Environments/Utils/GridWorld.cpp
@@ -0,0 +1,56 @@
+#include <AIToolbox/MDP/Environments/Utils/GridWorld.hpp>
+
+#include <algorithm>
+#include <cassert>
+
+namespace AIToolbox::MDP {
+    GridWorld::State::State(int xx, int yy, size_t ss) :
+        x(xx), y(yy), s(ss) {}
+
+    GridWorld::State::operator size_t() { return s; }
+
+    GridWorld::GridWorld(unsigned w, unsigned h) :
+            width_(w), height_(h)
+    {
+        assert(x > 0);
+        assert(y > 0);
+    }
+
+    GridWorld::State GridWorld::getAdjacent(Direction d, State s) {
+        switch ( d ) {
+            case UP:    return operator()(s.x, s.y-1);
+            case DOWN:  return operator()(s.x, s.y+1);
+            case LEFT:  return operator()(s.x-1, s.y);
+            case RIGHT: return operator()(s.x+1, s.y);
+        }
+    }
+
+    GridWorld::State GridWorld::operator()(int x, int y) const {
+        x = boundX(x);
+        y = boundY(y);
+        return State(x, y, static_cast<size_t>(x + y*width_));
+    }
+
+    GridWorld::State GridWorld::operator()(const size_t s) const {
+        return State(
+            std::min((int)s%width_, height_-1),
+            std::min((int)s/width_, height_-1),
+            s
+        );
+    }
+
+    int GridWorld::boundX(int x) const {
+        if ( x < 0 ) return 0;
+        if ( x >= (int)width_ ) return width_ - 1;
+        return x;
+    }
+
+    int GridWorld::boundY(int y) const {
+        if ( y < 0 ) return 0;
+        if ( y >= (int)height_ ) return height_ - 1;
+        return y;
+    }
+
+    unsigned GridWorld::getWidth() const { return width_; }
+    unsigned GridWorld::getHeight() const { return height_; }
+}
