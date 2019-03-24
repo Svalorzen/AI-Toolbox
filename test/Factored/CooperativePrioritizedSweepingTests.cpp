fda62a53dc03b2629cb702f4433528966c9001a8
Svalorzen <svalorzen@gmail.com>
2019-03-24 09:39:03 +0100

    Updates for debugging

--- test/Factored/CooperativePrioritizedSweepingTests.cpp
+++ test/Factored/CooperativePrioritizedSweepingTests.cpp
@@ -4,6 +4,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <AIToolbox/Utils/Core.hpp>
+#include <AIToolbox/Factored/MDP/Algorithms/LinearProgramming.hpp>
 #include <AIToolbox/Factored/MDP/Algorithms/CooperativePrioritizedSweeping.hpp>
 #include <AIToolbox/Factored/MDP/CooperativeExperience.hpp>
 #include <AIToolbox/Factored/MDP/CooperativeRLModel.hpp>
@@ -11,16 +12,24 @@
 #include <AIToolbox/Factored/MDP/Policies/EpsilonPolicy.hpp>
 #include <AIToolbox/Factored/MDP/Policies/QGreedyPolicy.hpp>
 
-#include "Utils/SysAdmin.hpp"
+#include <AIToolbox/Factored/MDP/Environments/SysAdmin.hpp>
 
+namespace ai = AIToolbox;
 namespace aif = AIToolbox::Factored;
 namespace fm = AIToolbox::Factored::MDP;
 
+template <typename T>
+std::ostream & operator<<(std::ostream & os, const std::vector<T> & v) {
+    for (auto vv : v)
+        os << vv << ' ';
+    return os;
+}
+
 BOOST_AUTO_TEST_CASE( simple_rule_update ) {
-    auto problem = makeSysAdminUniRing(2, 0.1, 0.2, 0.3, 0.4, 0.4, 0.4, 0.3);
+    auto problem = fm::makeSysAdminUniRing(2, 0.1, 0.2, 0.3, 0.4, 0.4, 0.4, 0.3);
 
     fm::CooperativeExperience exp(problem.getS(), problem.getA(), problem.getTransitionFunction().nodes);
-    fm::CooperativeRLModel model(exp, 0.9);
+    fm::CooperativeRLModel model(exp, 0.95);
 
     std::vector<std::vector<size_t>> domains{
         {0, 1},
@@ -35,7 +44,7 @@ BOOST_AUTO_TEST_CASE( simple_rule_update ) {
     aif::State s(model.getS().size());
     aif::Rewards r(model.getS().size());
     r.setZero();
-    for (size_t t = 0; t < 100; ++t) {
+    for (size_t t = 0; t < 1000; ++t) {
         std::cout << " #######\n";
         auto a = ep.sampleAction(s);
         auto [s1, x] = problem.sampleSR(s, a);
@@ -50,7 +59,38 @@ BOOST_AUTO_TEST_CASE( simple_rule_update ) {
         ps.batchUpdateQ();
         s = std::move(s1);
 
-        std::cout << ps.getQFunction().bases[0].values << '\n';
-        std::cout << ps.getQFunction().bases[1].values << '\n';
+        // std::cout << ps.getQFunction().bases[0].values << '\n';
+        // std::cout << ps.getQFunction().bases[1].values << '\n';
+    }
+
+    // Create and setup the bases to use for the ValueFunction.
+    auto vf = fm::ValueFunction();
+
+    for (size_t s = 0; s < problem.getS().size(); s += 2) {
+        for (size_t i = 0; i < 9; ++i) {
+            vf.values.bases.emplace_back(aif::BasisFunction{{s, s+1}, ai::Vector(9)});
+            vf.values.bases.back().values.setZero();
+            vf.values.bases.back().values[i] = 1.0;
+        }
+    }
+
+    auto solver = fm::LinearProgramming();
+
+    fm::QFunction q;
+    std::tie(vf.weights, q) = solver(problem, vf.values);
+
+    aif::PartialFactorsEnumerator se(problem.getS());
+    aif::PartialFactorsEnumerator ae(problem.getA());
+    while (se.isValid()) {
+        ae.reset();
+        while (ae.isValid()) {
+            std::cout << "State: " << se->second << "; Action: " << ae->second <<
+                         "; Q: " << q.getValue(problem.getS(), problem.getA(), se->second, ae->second) <<
+                         "; PS: " << ps.getQFunction().getValue(problem.getS(), problem.getA(), se->second, ae->second) <<
+                         "; Diff: " << q.getValue(problem.getS(), problem.getA(), se->second, ae->second) -
+                                     ps.getQFunction().getValue(problem.getS(), problem.getA(), se->second, ae->second) << "\n";
+            ae.advance();
+        }
+        se.advance();
     }
 }
