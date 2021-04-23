aa8318dcf3e6b566f65b34dfe266dc00496d7ce0
Svalorzen <svalorzen@gmail.com>
2021-04-23 15:23:11 +0200

    Factored Bandit FlattenedModel: added simple construction test

---
+++ test/Factored/Bandit/FlattenedModelTests.cpp
@@ -0,0 +1,36 @@
+#define BOOST_TEST_MODULE Factored_Bandit_FlattenedModel
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <AIToolbox/Factored/Bandit/Model.hpp>
+#include <AIToolbox/Factored/Bandit/FlattenedModel.hpp>
+
+namespace fb = AIToolbox::Factored::Bandit;
+
+BOOST_AUTO_TEST_CASE( construction ) {
+    AIToolbox::Factored::Action A{2,2,2};
+
+    std::vector<AIToolbox::Factored::PartialKeys> groups{{0,1},{1,2}};
+
+    AIToolbox::Bandit::Model<std::normal_distribution<double>> testa(std::vector<std::tuple<double, double>>{
+            {0.4, 1.0}, // 0-0
+            {-.5, 2.0}, // 1-0
+            {1.5, 1.0}, // 0-1
+            {-.7, 2.0}  // 1-1
+    });
+
+    AIToolbox::Bandit::Model<std::normal_distribution<double>> testb(std::vector<std::tuple<double, double>>{
+            {-.3, 1.0}, // 0-0
+            {-.5, 2.0}, // 1-0
+            {0.1, 1.0}, // 0-1
+            {-.9, 2.0}  // 1-1
+    });
+
+    fb::Model<std::normal_distribution<double>> bandit(std::move(A), std::move(groups), {std::move(testa), std::move(testb)});
+
+    fb::FlattenedModel flat(bandit);
+
+    BOOST_CHECK_EQUAL(flat.getA(), 8);
+}
