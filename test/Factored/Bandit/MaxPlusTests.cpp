ab5683ff5aeb2db2f68f3d16bfdf8a6e6f0ec5c6
Svalorzen <svalorzen@gmail.com>
2022-01-07 12:33:01 +0100

    MaxPlus: Using MininProblem to debug the algorithm

--- test/Factored/Bandit/MaxPlusTests.cpp
+++ test/Factored/Bandit/MaxPlusTests.cpp
@@ -5,6 +5,9 @@
 #include "GlobalFixtures.hpp"
 
 #include <AIToolbox/Factored/Bandit/Algorithms/Utils/MaxPlus.hpp>
+#include <AIToolbox/Factored/Bandit/Environments/MiningProblem.hpp>
+
+#include <iostream>
 
 namespace aif = AIToolbox::Factored;
 namespace fb = AIToolbox::Factored::Bandit;
@@ -132,4 +135,89 @@ BOOST_AUTO_TEST_CASE( negative_graph_2 ) {
     // BOOST_CHECK_EQUAL(val, solV);
     BOOST_CHECK_EQUAL_COLLECTIONS(std::begin(bestAction), std::end(bestAction),
                                   std::begin(solA),     std::end(solA));
+}
+
+template <typename T>
+std::ostream & operator<<(std::ostream & os, const std::vector<T> & v) {
+    std::cout << '[';
+    for (size_t i = 0; i < v.size() - 1; ++i)
+        std::cout << v[i] << ", ";
+    std::cout << v.back() << ']';
+    return os;
+}
+
+double rewFun(double productivity, size_t totalWorkers) {
+    assert(totalWorkers > 0);
+    return productivity * std::pow(1.03, totalWorkers);
+};
+
+BOOST_AUTO_TEST_CASE( mining_problem ) {
+    auto [A, workers, minePs] = fb::makeMiningParameters(0);
+
+    std::cout << "There are " << A.size() << " villages.\n";
+    std::cout << " - " << A << '\n';
+    std::cout << "The villages have the following worker counts:\n";
+    std::cout << " - " << workers << '\n';
+    std::cout << "There are " << minePs.size() << " mines.\n";
+    std::cout << "The mines have the following productivities:\n";
+    std::cout << " - " << minePs << '\n';
+
+    fb::MiningBandit bandit(A, workers, minePs);
+
+    std::cout << "Each mine is connected to the following villages:\n";
+    for (const auto & m : bandit.getGroups())
+        std::cout << " - " << m << '\n';
+
+    std::cout << "The optimal action for the problem is: " << bandit.getOptimalAction() << '\n';
+
+    const auto & groups = bandit.getGroups();
+    std::vector<fb::QFunctionRule> rules;
+    for (size_t m = 0; m < groups.size(); ++m) {
+        const auto & mineVillages = groups[m];
+
+        aif::PartialFactorsEnumerator enumerator(A, mineVillages);
+        while (enumerator.isValid()) {
+            const auto & villageAction = enumerator->second;
+
+            unsigned totalMiners = 0;
+            for (size_t v = 0; v < mineVillages.size(); ++v)
+                if (mineVillages[v] + villageAction[v] == m)
+                    totalMiners += workers[mineVillages[v]];
+
+            if (totalMiners > 0) {
+                const double v = rewFun(minePs[m], totalMiners);
+                rules.emplace_back(*enumerator, v);
+            }
+
+            enumerator.advance();
+        }
+    }
+
+    MP mp;
+    const auto [bestAction, val] = mp(A, rules);
+
+    std::cout << "MaxPlus thinks that the optimal action for the problem is: " << bestAction << '\n';
+    std::cout << "- Regret for this action is: " << bandit.getRegret(bestAction) << '\n';
+
+    double minRegret = std::numeric_limits<double>::max();
+    double maxRegret = std::numeric_limits<double>::lowest();
+    double avgRegret = 0.0;
+
+    AIToolbox::RandomEngine eng(100);
+    constexpr size_t toTry = 100;
+    for (size_t i = 0; i < toTry; ++i) {
+        const auto action = aif::makeRandomValue(A, eng);
+
+        const auto regret = bandit.getRegret(action);
+
+        minRegret = std::min(minRegret, regret);
+        maxRegret = std::max(maxRegret, regret);
+        avgRegret += regret;
+    }
+    avgRegret /= toTry;
+
+    std::cout << "I also tried " << toTry << " random actions to see how it compares.\n";
+    std::cout << " - Min regret: " << minRegret << '\n';
+    std::cout << " - Max regret: " << maxRegret << '\n';
+    std::cout << " - Avg regret: " << avgRegret << '\n';
 }
